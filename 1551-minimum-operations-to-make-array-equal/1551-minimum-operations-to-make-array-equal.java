7e3e7ba68a2d8284aac51a45197a19a13bb34d90
Sayan Saha <sahensha1600@gmail.com>
2022-07-13 09:46:08 +0530

    Time: 1 ms (88.09%), Space: 41.5 MB (31.16%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minOperations(int n) {
-        int cnt = n / 2;
-        return cnt * (cnt + n % 2);
+        int cunt=n/2;
+        return n%2==0?cunt*cunt:cunt*(cunt+1);
     }
 }
