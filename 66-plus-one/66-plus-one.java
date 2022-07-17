027b3577e6ac6282216a1a955ce90dfd71e06056
Sayan Saha <sahensha1600@gmail.com>
2022-07-17 11:43:58 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (61.47%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n =digits.length;
-        for(int i=n-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]++;
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]==9)
+            {
+                digits[i]=0;
+            }
+            else
+            {
+                digits[i]+=1;
                 return digits;
             }
-            digits[i]=0;
         }
-        
-        //for edge case like 99999 where  length of the array change 
-        int []new_number=new int[n+1];
-        new_number[0]=1;
-        return new_number;
+        int[] arr = new int[digits.length+1];
+        arr[0]=1;
+        return arr;
     }
 }
