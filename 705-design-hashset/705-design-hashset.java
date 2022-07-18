0d8ce17403e7071b55ea165a53d8470371b1f5da
Sayan Saha <sahensha1600@gmail.com>
2022-07-18 21:43:25 +0530

    Time: 845 ms (9.36%), Space: 57.5 MB (38.33%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+    static ArrayList a;
+    public MyHashSet() {
+        a = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        if(!a.contains(key))
+            a.add(key);
+    }
+    
+    public void remove(int key) {
+        if(a.contains(key))
+            a.remove(a.indexOf(key));
+    }
+    
+    public boolean contains(int key) {
+        return a.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
