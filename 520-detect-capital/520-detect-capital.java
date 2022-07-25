0d26631eb08074f8890b3d35f26267272ffcec9e
Sayan Saha <sahensha1600@gmail.com>
2022-07-25 13:08:02 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (55.64%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
         int count=0;
-        for(int i=0;i<word.length();i++){
-            char ch=word.charAt(i);
-            if(ch>='A'&&ch<='Z'){
+        for(int i=0;i<word.length();i++)
+        {char ch=word.charAt(i);
+            if(ch>='A' && ch<='Z')
+            {
                 count++;
+            }
         }
-    }
         if(count==word.length())
             return true;
         if(count==0)
             return true;
-        if(count==1 && word.charAt(0)>='A'&&word.charAt(0)<='Z')
+        if(count==1 && word.charAt(0)>='A' && word.charAt(0)<='Z')
             return true;
         else
             return false;
-}
+    }
 }
