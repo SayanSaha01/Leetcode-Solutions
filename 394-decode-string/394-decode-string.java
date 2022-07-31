a09047dc64f2cdb5755c32fa62efa00d3c1b4966
Sayan Saha <sahensha1600@gmail.com>
2022-07-31 09:43:31 +0530

    Time: 1 ms (88.72%), Space: 42 MB (58.07%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -7,16 +7,19 @@ class Solution {
         for (char ch : s.toCharArray()) {
             if (Character.isDigit(ch)) {
                 k = k * 10 + ch - '0';
-            } else if ( ch == '[') {
+            } 
+            else if ( ch == '[') {
                 intStack.push(k);
                 strStack.push(cur);
                 cur = new StringBuilder();
                 k = 0;
-            } else if (ch == ']') {
+            } 
+            else if (ch == ']') {
                 StringBuilder tmp = cur;
                 cur = strStack.pop();
-                for (k = intStack.pop(); k > 0; --k) cur.append(tmp);
-            } else cur.append(ch);
+                for (k = intStack.pop(); k > 0; k--) cur.append(tmp);
+            } 
+            else cur.append(ch);
         }
         return cur.toString();
     }
