484464915a1a2e15bd956fe708d80a6cbea62d39
Sayan Saha <sahensha1600@gmail.com>
2022-08-09 12:53:41 +0530

    Time: 10 ms (5.45%), Space: 47.2 MB (63.32%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -18,19 +18,21 @@ class Solution {
         ArrayList<Integer> values = new ArrayList<Integer>();
         ArrayList<TreeNode> tree = new ArrayList<TreeNode>();
         inorder(root,values,tree);
-        Collections.sort(values);
+        Collections.sort(values);//obtaining the correct order
         for(int i=0;i<values.size();i++)
         {
             tree.get(i).val = values.get(i);
         }
+     //storing the correct values in the nodes which earlier contained incorrect values
     }
     private void inorder(TreeNode root,ArrayList<Integer> values,ArrayList<TreeNode> tree)
     {
         if(root==null)
             return;
         inorder(root.left,values,tree);
-        values.add(root.val);
-        tree.add(root);
+        values.add(root.val); 
+//for storing the incorrect values, to be sorted to obtain the correct order
+        tree.add(root);    //for storing address of the nodes which have incorrect values
         inorder(root.right,values,tree);
     }
 }
