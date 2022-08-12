f832f68cac209ed8b90e47599f956665b1f86145
Sayan Saha <sahensha1600@gmail.com>
2022-08-12 10:28:18 +0530

    Time: 13 ms (73.44%), Space: 47.7 MB (5.45%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -5,10 +5,8 @@ class Solution {
         {
             for(int j=1;j<=word2.length();j++)
             {
-                if(word1.charAt(i-1) == word2.charAt(j-1)) 
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                else 
-                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
+                    : Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
         int val=dp[word1.length()][word2.length()];
