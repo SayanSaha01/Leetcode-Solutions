28bd45fee4ed1fd776011b12bce1fa1518fe7cee
Sayan Saha <sahensha1600@gmail.com>
2022-08-12 22:26:00 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (35.79%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length-1;i>=0;i--)
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
         {
-            if(digits[i]==9)
+            if(digits[i]<9)
             {
-                digits[i]=0;
-            }
-            else
-            {
-                digits[i]+=1;
+                digits[i]++;
                 return digits;
             }
+            digits[i]=0;
         }
-        int[] arr = new int[digits.length+1];
-        arr[0]=1;
-        return arr;
+        int[] newnum = new int[n+1];  //The last part of code is only for the case that the                                    // whole input array is 9s.For example : 99999-----> 100000
+                                         //Any other case would return in the loop.
+        newnum[0]=1;
+        return newnum;
     }
 }
