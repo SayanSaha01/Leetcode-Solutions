d7ae69ca9c9c61b6106a5ca969f42140375ab160
Sayan Saha <sahensha1600@gmail.com>
2022-08-22 09:06:07 +0530

    Time: 1 ms (83.56%), Space: 42.6 MB (8.10%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String s1="qwertyuiop",s2="asdfghjkl",s3="zxcvbnm";
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<words.length;i++)
+        {
+            int count1=0,count2=0,count3=0;
+            for(int j=0;j<words[i].length();j++)
+            {
+                if(s1.indexOf(words[i].toLowerCase().charAt(j))!=-1)
+                    count1++;
+                if(s2.indexOf(words[i].toLowerCase().charAt(j))!=-1)
+                    count2++;
+                if(s3.indexOf(words[i].toLowerCase().charAt(j))!=-1)
+                    count3++;
+            }
+            if((count1==words[i].length())||(count2==words[i].length())||(count3==words[i].length()))
+                list.add(words[i]);
+        }
+        return list.toArray(new String[0]);
+    }
+}
