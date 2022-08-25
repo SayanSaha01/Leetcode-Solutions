ac5f0312431a7f850701afedcef3d97f2a00a869
Sayan Saha <sahensha1600@gmail.com>
2022-08-25 10:10:33 +0530

    Time: 56 ms (5.10%), Space: 56.5 MB (69.06%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -8,7 +8,7 @@ class Solution {
                 pq.add(matrix[i][j]);
             }
         }
-        for(int i=1;i<k;i++)
+        for(int i=1;i<k;i++)     //if we iterate from 0 to <k it will go on till 8 which is 15 but we want 13 in the example 1 so we iterate from 1 to 7
         {
             pq.poll();
         }
