d634a249ba21a1a941882303854dea973decbd5d
Sayan Saha <sahensha1600@gmail.com>
2022-08-31 00:33:14 +0530

    Time: 15 ms (20.85%), Space: 43.5 MB (73.55%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -5,12 +5,14 @@ class Solution {
         {
             str[i]=String.valueOf(nums[i]);
         }
+// https://www.youtube.com/watch?v=qEIGhVtZ-sg for understanding the custom comparator 
         Arrays.sort(str,(o1,o2)->
         {
             String s1=o1+o2;
             String s2=o2+o1;
             return s2.compareTo(s1);
         });
+//this checks if the first string in the list of strings is 0 or not as we are sorting the array of strings in descending order if the max number stored correspondingly in the first index comes out to be 0 then the number will be  0 only  
         if(str[0].equals("0"))
            return str[0];
         StringBuilder sb = new StringBuilder();
