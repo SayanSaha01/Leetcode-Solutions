2ab49307ae6ff1d6d61d25f25284c05c6de16e83
Sayan Saha <sahensha1600@gmail.com>
2022-09-04 09:58:25 +0530

    Time: 0 ms (100.00%), Space: 59.3 MB (88.86%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,11 +1,34 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        if(arr.length>=3)
-            for(int i=1;i<arr.length-1;i++)
-            {
-                if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
-                    return i;
-            }
-        return -1;
+        return getindex(arr);
+    }
+    int getindex(int[] arr){
+        //If array has no elements
+        if(arr.length == 0){
+            return -1;
         }
+        
+        int start = 0;
+        int end = arr.length -1;
+        int target = 0;
+        
+        while(start < end){
+        
+            int mid = start + (end-start)/2;
+            //Comparing (mid) with (mid+1) 
+            target = arr[mid+1];
+            
+            if(target<arr[mid]){
+                // you are in decreasing part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid; 
+            }else if(target>arr[mid]){
+                // you are in ascending part of array
+                start = mid+1;
+            }
+        }
+        //In the end start = end, so you can return either start or end 
+        return end;
+    }
 }
