d3f528e0f0ab68719ac14d550d7a7297f9a1fbaf
Sayan Saha <sahensha1600@gmail.com>
2022-09-06 10:24:00 +0530

    Time: 1 ms (89.86%), Space: 44.3 MB (14.02%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,32 +1,14 @@
+//Bottom-Up Approach
 class Solution {
-
-    public int solve2(int[] cost,int n,int[] dp)
-    {
-        if(n<0)
-            return 0;
-        if(n==0||n==1){
-           dp[n] = cost[n];
-         return dp[n];
-        }
-        
-        //step3
-        if(dp[n]>0)
-            return dp[n];
-        
-        // the base cases remain same while converting a recursive solution to dp we need to make changes in the recursive relation
-        //step 2
-        dp[n] = cost[n]+Math.min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
-        return dp[n];
-    }
-    
     public int minCostClimbingStairs(int[] cost) {
-        //int n = cost.length;
-        //return Math.min(solve(cost,n-1),solve(cost,n-2));
-        
-        //Step 1 while creatinga dp
-        
-        int n= cost.length;
-        int[] dp = new int[n+1];
-        return Math.min(solve2(cost,n-1,dp),solve2(cost,n-2,dp));
+        int length = cost.length;
+        int[] dp = new int[length+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<length;i++)
+        {
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[length-1],dp[length-2]);
     }
 }
