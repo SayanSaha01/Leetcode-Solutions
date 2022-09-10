882d3919e5939a5826ed28c832e96c1cf5759067
Sayan Saha <sahensha1600@gmail.com>
2022-09-10 12:19:16 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (75.66%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -12,9 +12,11 @@
         if(n==0)
             return nums[0];
         
-        int include = recursion(nums,n-2)+nums[n];
+        int include = recursion(nums,n-2)+nums[n];   
         int exclude = recursion(nums,n-1)+0;
         
+//[1,2,3,1] => exclude is for the elements other than n-2 that is pairing of 1 with 1, 1 initially adds with 3 but if we exclude 3 we come to 1 which is also a non adjacent element
+
         return Math.max(include,exclude);
     }
 }*/
@@ -47,7 +49,7 @@
     }
 }*/
 
-//Tabulation
+/* Tabulation
 class Solution {
     public int rob(int[] nums) {
         return tabulation(nums);
@@ -67,7 +69,7 @@ class Solution {
         dp[1] = Math.max(nums[1],dp[0]);  /*we are getting this idea from the formula                 => Math.max(include,exclude) 
         => Math.max(dp[i-2] + nums[i],dp[i-1]);
         we are trying to find out the value of dp[i] where i=1 hence dp[i-2] doesnt exist hence we assume it to be zero 
-        =>dp[i] (i=1) = Math.max(nums[1],dp[0]);*/
+        =>dp[i] (i=1) = Math.max(nums[1],dp[0]);
         
         for(int i=2;i<n;i++)
         {
@@ -76,5 +78,31 @@ class Solution {
             dp[i] = Math.max(include,exclude);
         }
         return dp[n-1];
+    }
+}
+*/
+
+//Space Optimised
+class Solution {
+    public int rob(int[] nums) {
+        return optimised(nums);
+    }
+    public static int optimised(int[] nums)
+    {
+        int n=nums.length;
+        
+        int prev2=0;            //these values are obtained by looking at memoization  
+        int prev1=nums[0];
+        
+        for(int i=1;i<n;i++)
+        {
+            int incl = prev2 + nums[i];
+            int excl = prev1 + 0;
+            
+            int ans = Math.max(incl,excl);
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
     }
 }
