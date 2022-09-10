181c391587c686453544115be79eaaa7272e79cd
Sayan Saha <sahensha1600@gmail.com>
2022-09-10 12:55:23 +0530

    Time: 2 ms (6.31%), Space: 41.8 MB (27.19%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        ArrayList<Integer> arr1=new ArrayList<>();
+        ArrayList<Integer> arr2=new ArrayList<>();
+
+        if(n==1)
+           return nums[0];
+
+        for(int i=0; i<n; i++)
+        {
+            if(i!=0) 
+                arr1.add(nums[i]);
+            if(i!=n-1) 
+                arr2.add(nums[i]);
+        }
+        int ans1 = solve(arr1);
+        int ans2 = solve(arr2);
+
+        return Math.max(ans1,ans2);
+    }
+    static int solve(ArrayList<Integer> arr){
+    int n = arr.size();
+    int prev2 =0;
+    int prev1 = arr.get(0);
+        
+    for(int i=1; i<n; i++){
+        int  incl = arr.get(i);
+        if(i>1)
+            incl += prev2;
+        int excl = 0 + prev1;
+        
+        int ans = Math.max(incl, excl);
+        prev2 = prev1;
+        prev1 = ans;
+        
+       }
+    return prev1;
+    }
+}
