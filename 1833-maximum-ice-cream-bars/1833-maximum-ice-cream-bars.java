9540de42e00c942f88d5eaf91cc4eefd0cb76dc0
Sayan Saha <sahensha1600@gmail.com>
2022-09-10 22:21:34 +0530

    Time: 122 ms (5.00%), Space: 95.1 MB (5.50%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -6,9 +6,9 @@ class Solution {
             pq.add(i);
         }
         int count=0;
-       // while(coins>=0)      why this wont work if pq is empty then it will go inside the 
-                             //loop  and count will increment and show 1
-        while(!pq.isEmpty() && pq.peek()<=coins)
+       // while(coins>=0)      why this wont work if pq is empty and coins value is provided with something >0  then it will go inside the loop and count will increment and show 1
+        while(!pq.isEmpty() && pq.peek()<=coins)    
+//on the other hand if we only write(!pq.isEmpty()) then also it will throw error as suppose we reach the last and max element of the minheap then also it will go inside the loop and increment the count;
         {
             coins-=pq.poll();
             count++;
