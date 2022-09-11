0bf42b37c94e3fde3dda46102f0c8d512c433d35
Sayan Saha <sahensha1600@gmail.com>
2022-09-11 16:43:24 +0530

    Time: 2 ms (57.54%), Space: 40.5 MB (85.30%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -17,7 +17,7 @@ class Solution {
 }
 */
 //RECURSION => MEMOIZATION
-class Solution {
+/*class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target+1];
         Arrays.fill(dp,-1);
@@ -39,6 +39,27 @@ class Solution {
             ans+=memoization(nums,target-nums[i],dp);
         }
         dp[target] = ans;
+        return dp[target];
+    }
+}*/
+//TABULATION
+class Solution{
+    public int combinationSum4(int[] nums,int target)
+    {
+        int[] dp = new int[target+1];
+        dp[0]=1;
+        //for target array, consisting of numbers in range(target)
+        //traversing from 1 to target
+        for(int i=1;i<=target;i++)
+        {
+//j loop is basically iterating through the nums to achieve the target
+//traversing the num array            
+            for(int j=0;j<nums.length;j++)
+            {
+                if(i-nums[j]>=0)
+                   dp[i]+=dp[i-nums[j]];   //why dp[i-nums[j]] since i consists of numbers in range(target) and nums[j] consists of numbers which subtracted render the remaining target to be achieved in recursion
+            }
+        }
         return dp[target];
     }
 }
