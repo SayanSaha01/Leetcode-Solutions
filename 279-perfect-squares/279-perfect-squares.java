0ec569d7e999b43fc95cb949d82a08026a883cdb
Sayan Saha <sahensha1600@gmail.com>
2022-09-12 10:34:31 +0530

    Time: 72 ms (48.89%), Space: 43 MB (75.06%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -14,7 +14,7 @@
         return min;
     }
 }*/
-class Solution {
+/*class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
@@ -35,6 +35,22 @@ class Solution {
             min = Math.min(min,1+recursion(n-i*i,dp));
         }
         dp[n]=min;
+        return dp[n];
+    }
+}*/
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                if(i-j*j>=0)
+                   dp[i]=Math.min(dp[i],1+dp[i-j*j]);
+            }
+        }
         return dp[n];
     }
 }
