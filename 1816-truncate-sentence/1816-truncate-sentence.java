768b528d55303793795c55e7c1c512eaba43ce4c
Sayan Saha <sahensha1600@gmail.com>
2022-09-13 08:52:38 +0530

    Time: 1 ms (94.32%), Space: 42.4 MB (60.64%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String truncateSentence(String s, int k) {
         StringBuilder sb = new StringBuilder();
         String[] words = s.split(" ");
@@ -9,5 +9,20 @@ class Solution {
                 sb.append(" ");
         }
         return sb.toString();
+    }
+}*/
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+                count++;
+                if(count==k)
+                s=s.substring(0,i);
+            }
+        }
+        return s;
     }
 }
