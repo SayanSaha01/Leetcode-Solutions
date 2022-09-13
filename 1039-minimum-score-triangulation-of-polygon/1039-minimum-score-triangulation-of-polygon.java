dea43a11e2fa7fe6a982650406e8e8b9ee607e98
Sayan Saha <sahensha1600@gmail.com>
2022-09-13 22:50:02 +0530

    Time: 7 ms (25.32%), Space: 42.4 MB (13.21%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -18,6 +18,9 @@ class Solution {
     }
 }
 */
+//how do we figure out whether it is a 2D matrix or 1D matrix, check the number of variables changing 
+//RECURSION => TABULATION
+/*
 class Solution {
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
@@ -39,5 +42,33 @@ class Solution {
         }
         dp[i][j]=min;
         return dp[i][j];
+    }
+}
+*/
+//TABULATION - it is the exact opposite of Top Down approach(MEMOIZATION)
+class Solution {
+    public int minScoreTriangulation(int[] values) {
+        return tabulation(values);
+    }
+    public int tabulation(int[] v)
+    {   
+        int n=v.length;
+        int[][] dp = new int[n][n];
+        //In Top-Down dp we were starting i,j with 0 and n-1 but in Bottom up we are doing a bit different we start i from n-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+2;j<n;j++)   
+//if we start j=i it will be a single pointer and wont form a triangle
+//if we start from j=i+1 it will meet the base condition in memoization and recursion and return 0 so we start from i+2 and iterate till n 
+            {
+                int min = Integer.MAX_VALUE;
+                for(int k=i+1;k<j;k++)        //we iterate from >i to <j
+                {
+                    min = Math.min(min,(v[i]*v[j]*v[k]+dp[i][k]+dp[k][j]));
+                }
+                dp[i][j]=min; 
+            }      
+        }
+        return dp[0][n-1];          //as it bottom up, it is the reverse of top down
     }
 }
