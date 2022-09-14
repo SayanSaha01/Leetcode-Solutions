9bf243d0e808c89612216a15b2182230ce6ba52c
Sayan Saha <sahensha1600@gmail.com>
2022-09-14 21:06:05 +0530

    Time: 19 ms (86.89%), Space: 41.9 MB (95.61%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -25,6 +25,7 @@
     }
 }
 */
+/*
 class Solution {
     public int coinChange(int[] coins, int amount) 
     {
@@ -52,5 +53,24 @@ class Solution {
         }
         dp[amount]=min;
         return dp[amount];
+    }
+}*/
+class Solution {
+    public int coinChange(int[] coins, int amount) 
+    {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=0;i<=amount;i++)
+        {
+            for(int j=0;j<coins.length;j++)
+            {
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=Integer.MAX_VALUE)
+                {
+                    dp[i]=Math.min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }
