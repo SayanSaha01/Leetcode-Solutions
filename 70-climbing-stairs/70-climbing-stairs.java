fd3255e0a38a97a176387e4c0a1c410a5f7b6c75
Sayan Saha <sahensha1600@gmail.com>
2022-09-15 09:56:45 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (58.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,23 +1,38 @@
-//refer to this solution for understanding 
-//https://leetcode.com/problems/climbing-stairs/discuss/1968090/Java-from-Recursion-to-Bottom-up-(DP)-explained
-
+/*
 class Solution {
     public int climbStairs(int n) {
-
+        return recursion(n);
+    }
+    public int recursion(int n)
+    {
         if(n==0)
             return 0;
         if(n==1)
             return 1;
         if(n==2)
             return 2;
+        return recursion(n-1)+recursion(n-2);
+    }
+}
+*/
+class Solution{
+    public int climbStairs(int n)
+    {
         int[] dp = new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<dp.length;i++)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        Arrays.fill(dp,-1);
+        return memoization(n,dp);
+    }
+    public int memoization(int n,int[] dp)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n]= memoization(n-1,dp) + memoization(n-2,dp);
         return dp[n];
     }
 }
