f8be7359163ac67a57a8a6db31d75311fcbf9609
Sayan Saha <sahensha1600@gmail.com>
2022-09-15 10:04:44 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (83.28%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -15,6 +15,8 @@ class Solution {
     }
 }
 */
+//RECURSION => MEMOIZATION
+/*
 class Solution{
     public int climbStairs(int n)
     {
@@ -33,6 +35,24 @@ class Solution{
         if(dp[n]!=-1)
             return dp[n];
         dp[n]= memoization(n-1,dp) + memoization(n-2,dp);
+        return dp[n];
+    }
+}
+*/
+//TABULATION
+class Solution{
+    public int climbStairs(int n)
+    {
+        if(n==0||n==1||n==2)
+            return n;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 }
