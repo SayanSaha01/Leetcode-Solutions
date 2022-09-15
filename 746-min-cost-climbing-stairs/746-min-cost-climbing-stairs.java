1782a3a8702124ed0cdfb00875225f590b7b8424
Sayan Saha <sahensha1600@gmail.com>
2022-09-15 11:09:42 +0530

    Time: 2 ms (31.42%), Space: 43.9 MB (35.65%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,14 +1,37 @@
-//Bottom-Up Approach
+/*
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int length = cost.length;
-        int[] dp = new int[length+1];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<length;i++)
-        {
-            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
-        }
-        return Math.min(dp[length-1],dp[length-2]);
+        int n = cost.length;
+        return Math.min(recursion(cost,n-1),recursion(cost,n-2));
+        
+    }
+    public int recursion(int[] cost,int n)
+    {
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return cost[1];
+        int ans = cost[n] + Math.min(recursion(cost,n-1),recursion(cost,n-2));
+        return ans;
+    }
+}
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return Math.min(recursion(cost,n-1,dp),recursion(cost,n-2,dp));
+    }
+    public int recursion(int[] cost,int n,int[] dp)
+    {
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return cost[1];
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n] = cost[n] + Math.min(recursion(cost,n-1,dp),recursion(cost,n-2,dp));
+        return dp[n];
     }
 }
