7cf3a048c20d3169f24cd3ac13dd98e63ad4328b
Sayan Saha <sahensha1600@gmail.com>
2022-09-15 19:04:55 +0530

    Time: 1 ms (66.44%), Space: 42.3 MB (50.68%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -17,6 +17,7 @@ class Solution {
     }
 }
 */
+/*
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
@@ -38,5 +39,35 @@ class Solution {
             return dp[i][j];
         dp[i][j] = recursion(i-1,j,grid,dp) + recursion(i,j-1,grid,dp);
         return dp[i][j];
+    }
+}
+*/
+//TABULATION
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                    dp[i][j]=0;
+                else if(i==0 && j==0)
+                    dp[i][j]=1;
+                else
+                {
+                    int up = 0;
+                    int left = 0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        left = dp[i][j-1];
+                    dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
