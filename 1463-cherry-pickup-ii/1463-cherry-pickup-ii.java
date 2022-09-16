4f3670aeb99f48424348b2a539f927410efc61fe
Sayan Saha <sahensha1600@gmail.com>
2022-09-17 01:00:47 +0530

    Time: 70 ms (28.25%), Space: 46.8 MB (84.55%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -33,38 +33,38 @@ class Solution {
 */
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int r=grid.length;
-        int c=grid[0].length;
-        int dp[][][]=new int [r][c][c];
+        int m=grid.length;
+        int n=grid[0].length;
+        int dp[][][]=new int [m][n][n];
         for(int row[][]:dp){
             for(int col[]:row)
                 Arrays.fill(col,-1);
         }
-        return f(0,0,c-1,r,c,grid,dp);
+        return f(0,0,n-1,m,n,grid,dp);
     }
-    public static int f(int i1,int j1,int j2,int r,int c,int[][] grid,int[][][] dp){
-        if(j1<0 || j1>=c || j2<0 || j2>=c)
+    public static int f(int i,int j1,int j2,int m,int n,int[][] grid,int[][][] dp){
+        if(j1<0 || j1>=n || j2<0 || j2>=n)
             return (-1)*Integer.MAX_VALUE;
-        if(i1==r-1){
+        if(i==m-1){
             if(j1==j2)
-                return grid[i1][j1];
+                return grid[i][j1];
             else
-                return grid[i1][j1]+grid[i1][j2];
+                return grid[i][j1]+grid[i][j2];
         }
-        if(dp[i1][j1][j2]!=-1)
-            return dp[i1][j1][j2];
+        if(dp[i][j1][j2]!=-1)
+            return dp[i][j1][j2];
         int max=(-1)*Integer.MAX_VALUE;
         for(int dj1=-1;dj1<=1;dj1++){
             for(int dj2=-1;dj2<=1;dj2++){
                 int value=0;
                 if(j1==j2)
-                    value=grid[i1][j1];
+                    value=grid[i][j1];
                 else
-                    value=grid[i1][j1]+grid[i1][j2];
-                value+=f(i1+1,j1+dj1,j2+dj2,r,c,grid,dp);
+                    value=grid[i][j1]+grid[i][j2];
+                value+=f(i+1,j1+dj1,j2+dj2,m,n,grid,dp);
                 max=Math.max(max,value);
             }
         }
-        return dp[i1][j1][j2]=max;
+        return dp[i][j1][j2]=max;
     }
 }
