7f0ca0fe6181a0366207010622cb6606b507d4f6
Sayan Saha <sahensha1600@gmail.com>
2022-09-18 11:02:52 +0530

    Time: 278 ms (11.97%), Space: 82 MB (14.45%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,31 +1,32 @@
 class Solution {
-    Boolean mem[][];
     public boolean canPartition(int[] nums) {
         int sum = 0;
         int n = nums.length;
         
-        for(int i : nums) sum+=i;
+        for(int i : nums) 
+            sum+=i;
         
-        if(sum%2!=0) return false;
+        if(sum%2!=0) 
+            return false;
         
         sum /= 2;
         
-        mem = new Boolean[n+1][sum+1];
-        
-        return subsetSum(nums,0,sum);
+        Boolean[][] mem = new Boolean[n+1][sum+1];
+        return subsetSum(nums,0,sum,mem);
     }
     
-    boolean subsetSum(int[] nums, int pos, int sum){
-        if(sum==0) return true;
+    Boolean subsetSum(int[] nums, int pos, int sum,Boolean[][] mem){
+        if(sum==0) 
+            return true;
         
         else if(pos>=nums.length || sum<0) return false;
         
         if(mem[pos][sum]!=null) 
             return mem[pos][sum];
         
-        return mem[pos][sum] = subsetSum(nums,pos+1,sum-nums[pos]) ||
-                                subsetSum(nums,pos+1,sum);
+        Boolean take= subsetSum(nums,pos+1,sum-nums[pos],mem);
+        Boolean nottake = subsetSum(nums,pos+1,sum,mem);
         
-        
+        return mem[pos][sum] = take||nottake;
     }
 }
