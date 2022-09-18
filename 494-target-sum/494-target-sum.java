9bf87960eeba8b37359e800ef21accb11db1a1a0
Sayan Saha <sahensha1600@gmail.com>
2022-09-18 20:54:47 +0530

    Time: 13 ms (73.79%), Space: 41.6 MB (86.25%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int sum=0;
@@ -23,5 +24,41 @@ class Solution {
         if(nums[idx]<=target)
             pick = recursion(idx-1,target-nums[idx],nums);
         return (pick+notpick);
+    }
+}
+*/
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int d) {
+        int sum=0;
+        for(int i:nums)
+            sum+=i;
+        if(sum-d<0 || (sum-d)%2!=0)
+            return 0;
+        int n = nums.length;
+        int target = (sum-d)/2;
+        int[][] dp = new int[n][target+1];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+        return recursion(n-1,target,nums,dp);
+    }
+    public int recursion(int idx,int target,int[] nums,int[][] dp)
+    {
+        int mod = (int)Math.pow(10,9)+7;
+        if(idx==0)
+        {
+           if(target==0 && nums[idx]==0)
+              return 2;             
+           if(target == nums[idx] || target==0)
+              return 1;
+           return 0;
+        }
+        if(dp[idx][target]!=-1)
+            return dp[idx][target];
+        int notpick=recursion(idx-1,target,nums,dp);
+        int pick = 0;
+        if(nums[idx]<=target)
+            pick = recursion(idx-1,target-nums[idx],nums,dp);
+        return dp[idx][target]=(pick+notpick)%mod;
     }
 }
