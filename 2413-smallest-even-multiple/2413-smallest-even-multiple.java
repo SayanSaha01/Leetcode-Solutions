8fb064084162e104b764d2dbe736e2306e4f957d
Sayan Saha <sahensha1600@gmail.com>
2022-09-19 10:18:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (80.00%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,17 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        return lcm(n,2);
-    }
-    public int gcd(int a, int b)
-    {
-        if (a == 0)
-            return b;
-        return gcd(b % a, a);
-    }
-     
-    // method to return LCM of two numbers
-    public int lcm(int a, int b)
-    {
-        return (a / gcd(a, b)) * b;
+        if(n%2!=0)
+            return n*2;
+        else
+            return n;
     }
 }
