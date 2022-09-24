3d52e4cd8c622f886274ae8549e76e2c47e3ea68
Sayan Saha <sahensha1600@gmail.com>
2022-09-24 10:39:14 +0530

    Time: 100 ms (25.81%), Space: 44.8 MB (31.46%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -25,11 +25,13 @@ class Solution {
     }
 }
 */
+
 class Solution {
     public int lengthOfLIS(int[] arr){
         int n = arr.length;
         int dp[]=new int[n];
         Arrays.fill(dp,1);
+        int ans=-1;
         for(int i=0; i<=n-1; i++)
         {
             for(int prev_index = 0; prev_index <=i-1; prev_index ++)
@@ -39,11 +41,44 @@ class Solution {
                     dp[i] = Math.max(dp[i], 1 + dp[prev_index]);
                 }
             }
-        }
-        int ans = -1;
-        for(int i=0; i<=n-1; i++){
             ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
 }
+/*
+for printing the LIS
+import java.util.*;
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp=new int[n];
+    Arrays.fill(dp,1);
+    int[] hash=new int[n];
+    Arrays.fill(hash,1);  
+    for(int i=0; i<=n-1; i++){
+        hash[i] = i; // initializing with current index
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){           
+            if(arr[prev_index]<arr[i] && 1 + dp[prev_index] > dp[i]){
+                dp[i] = 1 + dp[prev_index];
+                hash[i] = prev_index;
+            }
+        }
+    }    
+    int ans = -1;
+    int lastIndex =-1;    
+    for(int i=0; i<=n-1; i++){
+        if(dp[i]> ans){
+            ans = dp[i];
+            lastIndex = i;
+        }
+    } 
+    ArrayList<Integer> temp=new ArrayList<>();
+    temp.add(arr[lastIndex]);
+    while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+        lastIndex = hash[lastIndex];
+        temp.add(arr[lastIndex]);    
+    }
+    Collections.reverse(temp);
+  }
+}*/
