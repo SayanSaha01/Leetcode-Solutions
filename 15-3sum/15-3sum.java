ec121686590e4c84d4f643d03c2d41c86b558916
Sayan Saha <sahensha1600@gmail.com>
2022-10-02 12:30:57 +0530

    Time: 1382 ms (7.53%), Space: 119.4 MB (17.71%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    Arrays.sort(nums);
-    
-    List<List<Integer>> ans = new LinkedList<>();
-    for(int i = 0; i<nums.length-2; i++){
-        if(i==0 || (i>0 && nums[i] != nums[i-1])){
-            int low = i+1, high = nums.length-1, sum = 0-nums[i];
-            
-            while(low<high){
-                if(nums[low] + nums[high] == sum){
-                    ans.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    
-                    while(low<high && nums[low] == nums[low + 1]) low++;
-                    while(low<high && nums[high] == nums[high - 1]) high--;
-                    low++;
-                    high--;
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++)
+        {
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum==0)
+                {
+                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
                 }
-                else if(nums[low] + nums[high] < sum) low++;
-                
-                else
-                    high--;
+                else if(sum>0){
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
             }
         }
+        return new ArrayList<>(set);
     }
-    return ans;
-   }
 }
