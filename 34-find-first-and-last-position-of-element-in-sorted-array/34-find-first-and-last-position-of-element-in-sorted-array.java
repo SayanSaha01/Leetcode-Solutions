3df04ede052b62854248a14b164737174954ce58
Sayan Saha <sahensha1600@gmail.com>
2022-10-03 23:15:57 +0530

    Time: 1 ms (42.93%), Space: 47.5 MB (29.05%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,58 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[2];
-        result[0] = lb(nums,target);
-        result[1] = ub(nums,target);
+        result[0]=first(nums,target);
+        result[1]=last(nums,target);
         return result;
     }
-    public int lb(int arr[],int target)
+    public int first(int[] nums,int target)
     {
         int start = 0;
-        int end = arr.length-1;
-        int idx=-1;
+        int end = nums.length-1;
+        int idx = -1;
         while(start<=end)
         {
-            int mid = start + (end-start)/2;
-            if(arr[mid]==target)
+            int mid = start+(end-start)/2;
+            if(target>nums[mid])
             {
-                idx=mid;
+                start = mid+1;
             }
-            if(arr[mid]>=target)
+            else if(target<nums[mid])
             {
                 end = mid-1;
             }
             else
             {
-                start = mid+1;
+                if(mid==0||nums[mid]!=nums[mid-1])
+                    return idx=mid;
+                else
+                    end=mid-1;
             }
         }
         return idx;
     }
-    public int ub(int arr[],int target)
+    public int last(int[] nums,int target)
     {
         int start = 0;
-        int end = arr.length-1;
-        int idx=-1;
+        int end = nums.length-1;
+        int idx = -1;
         while(start<=end)
         {
-            int mid = start + (end-start)/2;
-            if(arr[mid]==target)
+            int mid = start+(end-start)/2;
+            if(target>nums[mid])
             {
-                idx=mid;
+                start = mid+1;
             }
-            if(arr[mid]>target)
+            else if(target<nums[mid])
             {
                 end = mid-1;
             }
             else
             {
-                start = mid+1;
+                if(mid==nums.length-1||nums[mid]!=nums[mid+1])
+                    return idx=mid;
+                else
+                    start = mid+1;
             }
         }
         return idx;
