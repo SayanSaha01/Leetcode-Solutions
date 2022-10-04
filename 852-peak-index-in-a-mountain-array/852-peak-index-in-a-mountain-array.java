be647aae2da04b5bc3f542f8e5e528f19a8bd617
Sayan Saha <sahensha1600@gmail.com>
2022-10-04 22:02:58 +0530

    Time: 0 ms (100.00%), Space: 70.9 MB (83.89%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int peakIndexInMountainArray(int[] nums) {
         if(nums==null || nums.length<1)
             return -1;
@@ -25,5 +25,25 @@ class Solution {
                 end = mid-1;
         }
         return -1;
+    }
+}*/
+//MEthod 2 Love Babbar approach
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end)
+        {
+            int mid = start + (end-start)/2;
+            if(arr[mid]<arr[mid+1])
+            {
+                start = mid+1;
+            }
+            else  //this indicates that the peak element may lie on the second line or else it might be the peak element itself
+            {
+                end = mid;      //question is in all the questions we do mid-1, why not in this case. Answer-> if we do mid-1 it will go back to the first line but if we are in the else loop we are traversing through the 2nd line and it has to be on the second line
+            }
+        }
+        return start;
     }
 }
