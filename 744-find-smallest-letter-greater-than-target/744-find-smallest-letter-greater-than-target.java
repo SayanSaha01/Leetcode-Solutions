bee9251790d56031188f0cb72206fb1e9c6b8650
Sayan Saha <sahensha1600@gmail.com>
2022-10-06 09:05:55 +0530

    Time: 1 ms (66.29%), Space: 47.7 MB (68.86%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         for(char c:letters)
@@ -6,5 +7,37 @@ class Solution {
                 return c;
         }
         return letters[0];
+    }
+}
+/*the question says this -
+
+Note that the letters wrap around.
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+
+so if we cant find a greater char in array we return first element
+*/
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length-1;
+        int ans = 0;
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            if(letters[mid]==target)
+            {
+                start = mid+1;
+            }
+            else if(letters[mid]<target)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                ans = mid;
+                end = mid-1;
+            }
+        }
+        return letters[ans];
     }
 }
