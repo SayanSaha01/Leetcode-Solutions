470911fbe243e9b64549f4acf98ddd168f773579
Sayan Saha <sahensha1600@gmail.com>
2022-10-07 19:44:38 +0530

    Time: 5 ms (99.99%), Space: 43.9 MB (87.40%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,15 +9,22 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if( root==null || root==p || root==q)
+        
+        if(root==null)     //base case when we reach the null node
+            return null;
+        else if(root==p || root==q)//if we find the desired node, we return back the node
             return root;
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
-        if(left==null)
-            return right;
-        if(right==null)
-            return left;
+        
+        TreeNode left = lowestCommonAncestor(root.left,p,q);  //traversing the left subtree
+        TreeNode right = lowestCommonAncestor(root.right,p,q); //traversing the right subtree
+        
+        if(left!=null && right!=null)      //when we get the lowest common node from both the left and right subtree
+            return root;
+        
+        else if(left==null && right==null)   //when both the left and right subtree are null,then there is no chance of us getting back our desired node
+            return null;
+        
         else
-            return root;
+            return left!=null?left:right;     //when one of the node is null, we return the other node which is not null, to compare with the other node
     }
 }
