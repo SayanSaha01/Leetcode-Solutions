22c6767cbc7482ed6e802b246a5966b9a54a3369
Sayan Saha <sahensha1600@gmail.com>
2022-10-09 12:53:29 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (96.52%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,51 +1,31 @@
-/*class Solution {
+class Solution {
     public int findMin(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        
+        int pivot = findpivot(nums);
+        
+        if(pivot==-1)
+            return nums[0];
+        return nums[pivot+1];
+    }
+    public int findpivot(int[] nums)
+    {
         int start = 0;
         int end = nums.length-1;
-        int min = Integer.MAX_VALUE;
+        
         while(start<=end)
         {
-            int mid = start+(end-start)/2;
-            //checking which side is sorted
-            
-            //checking if the left side is sorted
-            if(nums[start]<=nums[mid])
-            {
-                if(nums[start]<min)
-                {
-                    min=nums[start];
-                }
+            int mid = start + (end-start)/2;
+            if(mid<end && nums[mid]>nums[mid+1])
+                return mid;
+            if(mid>start && nums[mid]<nums[mid-1])
+                return mid-1;
+            if(nums[start]>=nums[mid])
+                end=mid-1;
+            else
                 start=mid+1;
-            }
-            //checking if the right side is sorted
-            else
-            {
-                if(nums[mid]<min)
-                {
-                    min = nums[mid];
-                }
-                end = mid-1;
-            }
         }
-        return min;
-    }
-}*/
-class Solution {
-    public int findMin(int[] arr) {
-        int start =0;
-        int end = arr.length-1;
-        while(start<end)
-        {
-            int mid=start+(end-start)/2;
-            if(arr[mid]>arr[end])
-            {
-                start = mid+1;
-            }
-            else
-            {
-                end = mid;
-            }
-        }
-        return arr[start];
+        return -1;
     }
 }
