ca571f0041c96ff9656cbb5a40eaf1edaffaebed
Sayan Saha <sahensha1600@gmail.com>
2022-10-09 19:10:26 +0530

    Time: 2 ms (89.48%), Space: 43.1 MB (79.68%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -15,28 +15,31 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-    if (root == null) {
-            return 0;
-        }
-    LinkedList<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
-    root.val = 0;
-    int max = 1;
-    while (!queue.isEmpty()) {
-        int size = queue.size();
-        max = Math.max(max, queue.peekLast().val - queue.peekFirst().val + 1);
-        for (int i = 0; i < size; i++) {
-            root = queue.poll();
-            if (root.left != null) {
-                root.left.val = root.val * 2;
-                queue.offer(root.left);
+        ArrayDeque<TreeNode> dq = new ArrayDeque<TreeNode>();
+        root.val=0;
+        dq.offer(root);
+        int maxwidth=1;
+        while(!dq.isEmpty())
+        {
+            int size = dq.size();
+            int left = dq.peekFirst().val;
+            int right = dq.peekLast().val;
+            maxwidth = Math.max(maxwidth,(right-left+1));
+            for(int i=0;i<size;i++)
+            {
+                TreeNode node = dq.poll();
+                if(node.left!=null)
+                {
+                    node.left.val=2*node.val;
+                    dq.offer(node.left);
+                }
+                if(node.right!=null)
+                {
+                    node.right.val = 2*node.val+1;
+                    dq.offer(node.right);
+                }
             }
-            if (root.right != null) {
-                root.right.val = root.val * 2 + 1;
-                queue.offer(root.right);
-            }
         }
-    }
-    return max;
+        return maxwidth;
     }
 }
