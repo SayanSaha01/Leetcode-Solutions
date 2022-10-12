d5b1c6f892871b5ad8c257505dd7c9d546b81564
Sayan Saha <sahensha1600@gmail.com>
2022-10-12 16:57:15 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (63.49%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
@@ -6,6 +6,6 @@ class Solution {
             return checkPowersOfThree(n/3);
         if(n%3==1)
             return checkPowersOfThree((n-1)/3);
-        return false;
+        return false;    //when the remainder is 2, we cant think of 3^0 + 3^0 as in the question it is asked that we have to represent n as the sum of distinct powers of three while 3^0 , 3^0 arent distinct as they appear twice
     }
 }
