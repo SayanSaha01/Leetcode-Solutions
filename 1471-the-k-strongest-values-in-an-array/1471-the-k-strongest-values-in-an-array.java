a5571d2f600e549d44dbe6c018ccd4576c7b1415
Sayan Saha <sahensha1600@gmail.com>
2022-10-14 20:36:37 +0530

    Time: 236 ms (7.53%), Space: 95.5 MB (12.90%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.java
@@ -1,21 +1,11 @@
 class Solution {
     public int[] getStrongest(int[] arr, int k) {
         Arrays.sort(arr);
-        int n =arr.length;
-        int median = arr[(n-1)/2];
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(
-        (x,y)->Math.abs(y-median)!=Math.abs(x-median)?
-        Math.abs(y-median)-Math.abs(x-median):
-        y-x);
-        
-        for(int ele:arr)
-            pq.offer(ele);
-        int[] res = new int[k];
-        for(int i=0;i<k;i++)
-        {
-            res[i]=pq.poll();
-        }
-        return res;
+        int median = arr.length % 2 == 0 ? arr[arr.length/2 - 1] : arr[arr.length/2];
+        PriorityQueue<Integer> queue = new PriorityQueue<>((x,y) ->Math.abs(y-median)!=Math.abs(x-median) ? Math.abs(y-median) - Math.abs(x-median) : y-x);
+        for(int a: arr) queue.offer(a);
+        int[] result = new int[k];
+        for(int i=0; i < k; i++) result[i] = queue.poll();
+        return result;
     }
 }
