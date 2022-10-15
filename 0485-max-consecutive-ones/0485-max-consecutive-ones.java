8bb6890ab66eca5ff497a379c7028b9045a05bcd
Sayan Saha <sahensha1600@gmail.com>
2022-10-15 22:32:40 +0530

    Time: 6 ms (27.48%), Space: 57.9 MB (5.52%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,3 +1,6 @@
+//the end pointer iterates over the length of the array, and the start pointer starts at the beginning of the array. Whenever I am still on a 1, I update max, and once I hit a 0, I move the start of the window to the element after the current end. This is because we know that the current end is 0, so we move the start to the next element that could be a 1, which is the one after the current end.
+
+//dry run to understand
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max=0;
