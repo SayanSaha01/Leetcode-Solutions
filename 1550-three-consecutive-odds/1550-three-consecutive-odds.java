61010faaf12aae0537a09358757d4fc02e84ee7e
Sayan Saha <sahensha1600@gmail.com>
2022-10-17 17:33:00 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (73.69%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,9 +1,23 @@
+/*class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0;i<arr.length-2;i++)
+        {
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0)
+                return true;
+        }
+        return false;
+    }
+}*/
+
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int count=0;
-        for(int i=0;i<arr.length;i++)
+        for(int i:arr)
         {
-            count=arr[i]%2==1? count+1:0;
+            if(i%2!=0)
+                count++;
+            else
+                count=0;
             if(count==3)
                 return true;
         }
