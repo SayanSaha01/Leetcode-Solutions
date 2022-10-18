82c2ae09227c2c54184ae6a0b982ee937ac4779b
Sayan Saha <sahensha1600@gmail.com>
2022-10-18 10:47:34 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (77.49%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,69 +1,46 @@
-/*class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int left = 0;
-        int right = matrix[0].length-1;
-        int start = 0;
-        int bottom = matrix.length-1;
-        while(start<=bottom && left<=right)
-        {
-            // Top Right row
-            for(int i=left;i<=right;i++)
-            {
-                list.add(matrix[start][i]);
-            }
-            start++;
-            // Right Down column
-            for(int i=start;i<=bottom;i++)
-            {
-                list.add(matrix[i][right]);
-            }
-            right--;
-            // Bottom Left row
-            if(start<=bottom)
-            {
-                for(int i=right;i>=left;i--)
-                {
-                    list.add(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            // Left Up row
-           if(left<=right)
-           {
-                for(int i=bottom;i>=start;i--)
-                {
-                    list.add(matrix[i][left]);
-                }
-                left++;
-           }
-        }
-        return list;
-    }
-}*/
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new LinkedList<>(); 
-        if (matrix == null || matrix.length == 0) return res;
-        int n = matrix.length, m = matrix[0].length;
-        int up = 0,  down = n - 1;
-        int left = 0, right = m - 1;
-        while (res.size() < n * m) {
-            for (int j = left; j <= right && res.size() < n * m; j++)
-                res.add(matrix[up][j]);
+        List<Integer> list = new ArrayList<Integer>();
+        int tne=matrix.length * matrix[0].length;
+        int startrow = 0;
+        int bottomrow = matrix.length-1;
+        int leftcol = 0;
+        int rightcol = matrix[0].length-1;
+        int count=0;
+        while(count<tne)
+        {
+            //top row
+            for(int i=leftcol;i<=rightcol && count<tne;i++)
+            {
+                list.add(matrix[startrow][i]);
+                count++;
+            }
+            startrow++;
             
-            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
-                res.add(matrix[i][right]);
-                     
-            for (int j = right; j >= left && res.size() < n * m; j--)
-                res.add(matrix[down][j]);
-                        
-            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--) 
-                res.add(matrix[i][left]);
-                
-            left++; right--; up++; down--; 
+            // right column
+            for(int i=startrow;i<=bottomrow && count<tne;i++)
+            {
+                list.add(matrix[i][rightcol]);
+                count++;
+            }
+            rightcol--;
+            
+            // bottom row
+            for(int i=rightcol;i>=leftcol && count<tne; i--)
+            {
+                list.add(matrix[bottomrow][i]);
+                count++;
+            }
+            bottomrow--;
+            
+            // left col
+            for(int i=bottomrow;i>=startrow && count<tne;i--)
+            {
+                list.add(matrix[i][leftcol]);
+                count++;
+            }
+            leftcol++;
         }
-        return res;
+        return list;
     }
 }
