e4e86f5991439df814f590d8dc622b35d7ed4451
Sayan Saha <sahensha1600@gmail.com>
2022-10-19 09:12:59 +0530

    Time: 13 ms (47.54%), Space: 45.4 MB (32.96%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        for(int i=0;i<words.length;i++)
+        {
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
+        }
+        //if 2 words have the same frequency then we take the lexicographically first ordering word else we tae the words having higher frequency
+        PriorityQueue<String> pq = new PriorityQueue<>(
+            (a,b)->map.get(a).equals(map.get(b))
+            ?b.compareTo(a)
+            :map.get(a)-map.get(b)
+        );
+        for(String word:map.keySet())
+        {
+            pq.add(word);
+            if(pq.size()>k)
+                pq.remove();
+        }
+        List<String> list = new ArrayList<>();
+        for(int i=1;i<=k;i++)
+            list.add(0,pq.remove());
+        return list;                                             
+    }
+}
