e81dfb15db1e54ef7afdd96a1981a971f1a0fccd
Sayan Saha <sahensha1600@gmail.com>
2022-10-22 11:28:27 +0530

    Time: 23 ms (25.24%), Space: 44.7 MB (51.20%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-        int[] numscopy = new int[nums.length];
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int i=0;i<nums.length;i++)
-        {
-            numscopy[i] = nums[i];
-            pq.offer(nums[i]);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();  //minheap
         ArrayList<Integer> list = new ArrayList<Integer>();
+     
+        for(int i:nums)
+        {
+            pq.add(i);
+            list.add(i);
+        }
+        
+        for(int i=0;i<nums.length-k;i++)   //we only need k elements, so dropping the lowest elements,leaving the elements which render the max sum, the list mantains their natural order
+        {
+            list.remove(pq.poll());
+        }
+        int[] arr = new int[k];
         for(int i=0;i<k;i++)
         {
-            list.add(pq.poll());
-        }
-        int arr[] = new int[k];
-        int index=0;
-        for(int i=0;i<numscopy.length;i++)
-        {
-            if(list.contains(numscopy[i]))
-            {
-                arr[index]=numscopy[i];
-                index++;
-                list.remove(list.indexOf(numscopy[i]));
-            }
+            arr[i]=list.get(i);
         }
         return arr;
     }
