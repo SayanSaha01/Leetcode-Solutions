f5200168dd8fc13529d9659481ef4239ac5c38f3
Sayan Saha <sahensha1600@gmail.com>
2022-10-24 11:51:47 +0530

    Time: 21 ms (100.00%), Space: 41.9 MB (100.00%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
@@ -1,11 +1,16 @@
+//Time Complexity - 0(n^2)
 class Solution {
     public int subarrayGCD(int[] nums, int k) {
         int count=0;
+        // consider every element as a starting point 
         for(int i=0;i<nums.length;i++)
         {
+         // since gcd of nums[i] is nums[i] itself we initialize curr_gcd with 0 
             int curr_gcd=0;
+         // from the given point try all the subarrays 
             for(int j=i;j<nums.length;j++)
             {
+                // keep updating the curr_gcd
                 curr_gcd=gcd(nums[j],curr_gcd);
                 if(curr_gcd==k)
                     count++;
