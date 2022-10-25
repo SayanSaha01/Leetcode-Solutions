6ba8db59915716f5dafca49427503fc5529ddadc
Sayan Saha <sahensha1600@gmail.com>
2022-10-25 16:09:34 +0530

    Time: 1422 ms (21.98%), Space: 75.6 MB (57.05%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -2,9 +2,30 @@ class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
         for(int i = 0;i<nums.length;i++) {
             for(int j = i+1;j<=i+indexDiff && j<nums.length;j++){
-                if(Math.abs(nums[i] - nums[j]) <=valueDiff) return true;
+                if(Math.abs(nums[i] - nums[j]) <=valueDiff) 
+                    return true;
             }
         } 
         return false;
     }
 }
+/* public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {        
+        int leftIndex = 0;
+        int rightIndex = 1;
+        
+        while(rightIndex < nums.length){
+            int tempValueDiff = Math.abs(nums[leftIndex] - nums[rightIndex]);
+            int tempIndexDiff = Math.abs(leftIndex - rightIndex);
+            
+            if(tempValueDiff <= valueDiff && tempIndexDiff <= indexDiff){
+               return true;
+            }            
+            else if(tempIndexDiff == indexDiff || rightIndex == nums.length - 1){
+                rightIndex = ++leftIndex;
+            }
+            rightIndex++;
+        }
+        
+        return false;
+    }
+}*/
