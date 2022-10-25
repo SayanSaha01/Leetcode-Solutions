3fc948646f9ec2db383a474980f1bf6422d61892
Sayan Saha <sahensha1600@gmail.com>
2022-10-25 16:07:35 +0530

    Time: 1621 ms (16.68%), Space: 75.2 MB (61.20%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        for(int i = 0;i<nums.length;i++) {
+            for(int j = i+1;j<=i+indexDiff && j<nums.length;j++){
+                if(Math.abs(nums[i] - nums[j]) <=valueDiff) return true;
+            }
+        } 
+        return false;
+    }
+}
