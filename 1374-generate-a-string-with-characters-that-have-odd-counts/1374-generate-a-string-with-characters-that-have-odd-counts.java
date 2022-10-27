77213cf5e27519a7c739876c08a904bdd7cdc649
Sayan Saha <sahensha1600@gmail.com>
2022-10-27 19:01:34 +0530

    Time: 1 ms (96.31%), Space: 41.8 MB (47.12%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String generateTheString(int n) {
+        char[] arr = new char[n];
+        Arrays.fill(arr,'a');
+        if(n%2==0)
+        {
+            arr[0]='b';
+        }
+        return new String(arr);
+    }
+}
