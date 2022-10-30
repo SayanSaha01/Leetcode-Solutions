62480fd19cca6ec829316de3a10e1b7ceedf9dee
Sayan Saha <sahensha1600@gmail.com>
2022-10-30 11:34:53 +0530

    Time: 23 ms (25.00%), Space: 42.2 MB (50.00%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
-        ArrayList<String> list = new ArrayList<String>();
-        for(int i=0;i<queries.length;i++)
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<queries.length;i++)  //iterating over queries first as we need to mantain the relative order
         {
-            String s = queries[i];
+            String word = queries[i];
             for(int j=0;j<dictionary.length;j++)
             {
-                int count=0;
-                String str = dictionary[j];
-                for(int k=0;k<str.length();k++)
+                int count=0;    //for every word in dictionary we are checking the number of mismatching letters
+                String dict = dictionary[j];
+                for(int k=0;k<dict.length();k++)
                 {
-                    if(str.charAt(k)!=s.charAt(k))
+                    if(word.charAt(k)!=dict.charAt(k))
                     {
                         count++;
                     }
                 }
                 if(count<=2)
                 {
-                    list.add(s);
+                    list.add(word);
                     break;
                 }
             }
