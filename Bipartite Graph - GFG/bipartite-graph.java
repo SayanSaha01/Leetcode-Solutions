d4db4a8b8a40da5b313f47c15e9033856a47a7f9
Sayan Saha <sahensha1600@gmail.com>
2022-10-31 16:57:13 +0530

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(S[0]);
+            int E = Integer.parseInt(S[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for(int i = 0; i < V; i++){
+                adj.add(new ArrayList<Integer>());
+            }
+            for(int i = 0; i < E; i++){
+                String[] s = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(V, adj);
+            if(ans)
+                System.out.println("1");
+            else System.out.println("0");
+       }
+    }
+}
+// } Driver Code Ends
+class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int[] vertices = new int[V];
+        for(int i=0;i<V;i++)
+        {
+            vertices[i] = -1;
+        }
+        for(int i=0;i<V;i++)
+        {
+            if(vertices[i]==-1)
+            {
+                if(dfs(i,0,vertices,adj)==false)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int i,int col, int[] vertices, ArrayList<ArrayList<Integer>>adj)
+    {
+        vertices[i]=col;
+        for(int it:adj.get(i))
+        {
+            if(vertices[it]==-1)
+            {
+                if(dfs(it,1-col,vertices,adj)==false)
+                {
+                    return false;
+                }
+            }
+            else if(vertices[it]==col)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
