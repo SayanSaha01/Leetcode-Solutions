04e56c20ca326475ecac81eacdd0ce44638cf0e8
Sayan Saha <sahensha1600@gmail.com>
2022-11-04 10:56:27 +0530

    Time: 10 ms (49.08%), Space: 45.4 MB (34.98%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,37 @@
 class Solution {
     public String reverseVowels(String s) {
-        int start = 0;
-        int end = s.length()-1;
-        String vowel="aeiouAEIOU";
+
         char[] ch = s.toCharArray();
+        int start=0;
+        int end = s.length()-1;
+        HashSet<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('O');
+        set.add('I');
+        set.add('U');
         while(start<end)
         {
-            if(!vowel.contains(ch[start]+""))
+            if((set.contains(ch[start])) && (set.contains(ch[end])))
             {
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
                 start++;
+                end--;
             }
-            if(!vowel.contains(ch[end]+""))
+            else if((set.contains(ch[start])) && (!set.contains(ch[end])))
             {
                 end--;
             }
-            if(vowel.contains(ch[start]+"") && vowel.contains(ch[end]+""))
+            else
             {
-                char temp=ch[start];
-                ch[start]=ch[end];
-                ch[end]=temp;
                 start++;
-                end--;
             }
         }
         return new String(ch);
