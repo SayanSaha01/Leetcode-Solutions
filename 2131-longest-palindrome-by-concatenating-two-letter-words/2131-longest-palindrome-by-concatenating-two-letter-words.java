e71b43959ca5f333b968e514e05209f29bace934
Sayan Saha <sahensha1600@gmail.com>
2022-11-04 17:24:48 +0530

    Time: 475 ms (7.97%), Space: 126.6 MB (10.94%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,42 +1,38 @@
 class Solution {
-    public String revstring(String st)
+    public String revstring(String str)
     {
         String rev="";
-        for(int i=st.length()-1;i>=0;i--)
+        for(int i=str.length()-1;i>=0;i--)
         {
-            rev+=st.charAt(i);
+            rev+=str.charAt(i);
         }
         return rev;
     }
     public int longestPalindrome(String[] words) {
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
         int max=0;
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        
         for(String str:words)
         {
-            String flipped = revstring(str);
-            //as it is mentioned in the question each word is of length two , hence to find the palindrome we are adding charAt(1)+charAt(0)
-            if(map.containsKey(flipped))
+            String rev = revstring(str);
+            if(map.containsKey(rev))
             {
-                max += 4; 
-//why we are adding 4 -> suppose word is ab then its flipped is ba and if String[] words contains ab and ba then the length of the palindromic string will be 4 (ab......ba)
-                
-                map.put(flipped,map.get(flipped)-1);
-    //we dont know the number of times, flipped occurs in map hence reducing its count by 1, if somehow value of flipped becomes 0 in map we remove it so that it doesnt return 0 everytime
-                if(map.get(flipped)==0)
-                {
-                    map.remove(flipped);
-                }
+                max+=4;
+                map.put(rev,map.get(rev)-1);
+                if(map.get(rev)==0)
+                    map.remove(rev);
             }
             else
-    //if the map doesnt contain the reverse of a word then we just add the original word in map
             {
                 map.put(str,map.getOrDefault(str,0)+1);
             }
         }
         
+        // for words in String words such as gg which is itself the reverse of itself it would have a palindrome but needs to be added to the string at the centre, 
         for(String s:map.keySet())
         {
-            if(map.get(s)==1 && (s.equals(revstring(s))))
+        //the map would also contains redundant words such as ab which might have not any reverse present in words of ab such as ba, so we need to check if the word s in string is in itself a reverse of it or not, if yes we add +2 to it, there might be multiple words such as aa, xx ,yy present, we need to add only 1 such instance as all others would yield the same max length only
+            if(s.equals(revstring(s)))
             {
                 max+=2;
                 break;
