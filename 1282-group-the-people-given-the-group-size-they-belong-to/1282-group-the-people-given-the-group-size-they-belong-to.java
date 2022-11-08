ef41743e9923c919d83f5384d21398b532dbbe4d
Sayan Saha <sahensha1600@gmail.com>
2022-11-08 12:30:23 +0530

    Time: 11 ms (74.05%), Space: 54.3 MB (26.85%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,18 +1,20 @@
 class Solution {
-    public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        List<List<Integer>> res = new ArrayList<>();
+     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
         int n = groupSizes.length;
-        List<Integer> arr[] = new ArrayList[n+1];
-        for(int i = 1; i <= n; i++)
-            arr[i] = new ArrayList<Integer>();
-        for(int i=0;i<n;i++){
-            int x = groupSizes[i];
-            arr[x].add(i);
-            if(arr[x].size() == x){
-                res.add(arr[x]);
-                arr[x] = new ArrayList<>();
-            }   
+        for (int i=0; i<n; i++) {
+            int current = groupSizes[i];
+            List<Integer> temp = new ArrayList<>();
+            if (map.containsKey(current))
+                temp = map.get(current);
+            temp.add(i);
+            map.put(current, temp);
+            if (temp.size() == current) {
+                result.add(temp);
+                map.remove(current);
+            }
         }
-        return res;
+        return result;
     }
 }
