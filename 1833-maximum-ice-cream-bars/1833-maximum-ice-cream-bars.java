631b3acf7e3fef3c40cbb600b9947bf3cec29221
Sayan Saha <sahensha1600@gmail.com>
2022-11-08 18:48:08 +0530

    Time: 86 ms (5.38%), Space: 95.3 MB (5.11%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,34 +1,16 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int count=0;
-        for(int cost:costs)
-        {
-            if(cost>coins)
-                break;
-            else
-                coins-=cost;
-                count++;
-        }
-        return count;
-    }
-}
-/*
-class Solution {
-    public int maxIceCream(int[] costs, int coins) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        int count=0;
         for(int i:costs)
         {
-            pq.add(i);
+            pq.offer(i);
         }
-        int count=0;
-       // while(coins>=0)      why this wont work if pq is empty and coins value is provided with something >0  then it will go inside the loop and count will increment and show 1
-        while(!pq.isEmpty() && pq.peek()<=coins)    
-//on the other hand if we only write(!pq.isEmpty()) then also it will throw error as suppose we reach the last and max element of the minheap then also it will go inside the loop and increment the count;
+        while(!pq.isEmpty() && pq.peek()<=coins)
         {
             coins-=pq.poll();
             count++;
         }
         return count;
     }
-}*/
+}
