6d8f2d36d35d03af0e3509dd94f13d1909be5f1c
Sayan Saha <sahensha1600@gmail.com>
2022-11-10 13:02:51 +0530

    Time: 13 ms (84.04%), Space: 45.4 MB (40.70%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,3 +1,25 @@
+/*just maintain three pointers to hold most recent indices of a,b and c . then the number of substrings at any index lets say with char c is the most closest index to left from where a and b are found +1 for the new substring .
+       len = 11
+       index-> 0 1 2 3 4 5 6 7 8 9 10
+           s-> c b c b c c b  b a a a
+      
+     c   -> 0
+     cb  -> 0
+     cbc -> 0
+     cbcb ->0
+     cbcbc->0
+     cbcbcc->0
+     cbcbccb->0
+     cbcbccbb->0
+     cbcbccbba->1      // as it is mentioned substring containing at least one                           occurence of all three characters
+     bcbccbba->2
+     cbccbba->3
+     bccbba->4
+     ccbba->5
+     cbba ->6   in this case the most recent index of b and c are 6 and 5 so min(6,5)+1
+    
+    */
+    
 class Solution {
     public int numberOfSubstrings(String s) {
         int substrings=0;
