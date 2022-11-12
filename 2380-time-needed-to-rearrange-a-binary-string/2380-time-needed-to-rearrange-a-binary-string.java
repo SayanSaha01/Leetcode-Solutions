2450633b2793bac45e109686cfecc06ea8310ff3
Sayan Saha <sahensha1600@gmail.com>
2022-11-12 23:17:16 +0530

    Time: 240 ms (33.62%), Space: 115.2 MB (25.00%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.java
@@ -1,21 +1,9 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        char[] c = s.toCharArray();
         int count=0;
         while(s.contains("01"))
         {
-            for(int i=0;i<c.length-1;i++)
-            {
-                if(c[i]=='0' && c[i+1]=='1')
-                {
-                    char temp = c[i];
-                    c[i]=s.charAt(i+1);
-                    c[i+1]=temp;
-                    i++;
-                }
-            }
-            s = new String(c);
-            c = s.toCharArray();
+            s=s.replace("01","10");
             count++;
         }
         return count;
