2d85c3e49937d504abb57e878d0f5e253b21a722
Sayan Saha <sahensha1600@gmail.com>
2022-11-12 15:00:25 +0530

    Time: 1 ms (76.95%), Space: 43.4 MB (69.79%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -17,14 +17,9 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null)
             return false;
-        /*if(root.val==targetSum)
-            return true;  
-this doesnt work, as it maybe the first node itself, it may satisfy the condition but it may have some left and right leaf nodes. 
-Consider the cases root = 5, sum=5 it will satisfy the condition but if
-[5,2,3] and sum=5 then it wont satisfy as 5-2 & 5-3 is a path, we cant just break and take a node out of the root-to-leaf path and call it a path, for the condition to satisfy the node should have the left and right nodes as null then only we can say it satisfies the condition root-to-leaf.*/ 
-        if(root.left==null && root.right==null && root.val==targetSum)
+        if(root.left==null && root.right==null && targetSum-root.val==0)
             return true;
-        return hasPathSum(root.left,targetSum-root.val) || 
+        return hasPathSum(root.left,targetSum-root.val)||
                hasPathSum(root.right,targetSum-root.val);
     }
 }
