94ab3451838481e38c391b62eb8eb5edc7144278
Sayan Saha <sahensha1600@gmail.com>
2022-11-13 22:43:18 +0530

    Time: 70 ms (32.87%), Space: 161.1 MB (53.98%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 1;i< nums.length-1;i+=2){
-            swap(nums,i,i+1);
+        for(int i=1;i<nums.length-1;i+=2)
+        {
+            int temp = nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
         }
         return nums;
-    }
-    public void swap(int arr[],int i,int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        return;
     }
 }
