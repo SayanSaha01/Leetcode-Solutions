92e2d5e93cff4aa65112af2713f26eb147a52d87
Sayan Saha <sahensha1600@gmail.com>
2022-11-14 18:46:30 +0530

    Time: 319 ms (37.16%), Space: 75.5 MB (51.13%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -3,21 +3,22 @@ class Solution {
         TreeMap<Integer,Integer> treemap = new TreeMap<Integer,Integer>();
         for(int i:nums)
         {
+            //storing in treemap based on frequency
             treemap.put(i,treemap.getOrDefault(i,0)+1);
         }
         while(treemap.size()!=0)
         {
             int val = treemap.firstKey();
-            for(int i=val;i<val+k;i++)
+            for(int i=val;i<val+k;i++)  //val+k will be our window size
             {
-                if(!treemap.containsKey(i))
+                if(!treemap.containsKey(i))  //if map dose not contain consecutive number within window
                 {
                     return false;
                 }
                 else
                 {
                     treemap.put(i,treemap.get(i)-1);
-                    if(treemap.get(i)==0)
+                    if(treemap.get(i)==0)   //removed completly coz it is now used in above set
                         treemap.remove(i);
                 }
             }
