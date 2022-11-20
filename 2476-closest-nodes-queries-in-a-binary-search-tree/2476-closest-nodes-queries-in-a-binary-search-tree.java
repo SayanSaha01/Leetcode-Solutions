7e062dd5e1950bca03a051ffc0bf435cbe4dcb17
Sayan Saha <sahensha1600@gmail.com>
2022-11-20 10:59:52 +0530

    Time: 409 ms (25.00%), Space: 188.7 MB (50.00%) - LeetHub

---
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+        TreeMap<Integer,Integer> tmap = new TreeMap<>();
+        List<List<Integer>> list = new ArrayList<>();
+        
+        sorted(root,tmap);
+        for(int i:queries)
+        {
+            List<Integer> sublist = new ArrayList<>();
+            Integer lowerkey = tmap.containsKey(i) ? Integer.valueOf(i) : tmap.lowerKey(i);
+            Integer higherkey = tmap.containsKey(i) ? Integer.valueOf(i) : tmap.higherKey(i);
+            
+            sublist.add(lowerkey==null ? -1:lowerkey);
+            sublist.add(higherkey==null ? -1:higherkey);
+            
+            list.add(sublist);
+        }
+        return list;
+    }
+    public void sorted(TreeNode root,TreeMap<Integer,Integer> tmap)
+    {
+        if(root==null)
+            return;
+        sorted(root.left,tmap);
+        tmap.put(root.val,1);
+        sorted(root.right,tmap);
+    }
+}
