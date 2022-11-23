1616acd9af9904a3c363dd8b61e2556f686da224
Sayan Saha <sahensha1600@gmail.com>
2022-11-23 23:05:16 +0530

    Time: 24 ms (38.32%), Space: 48.5 MB (48.61%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.java
@@ -9,43 +9,47 @@
  */
 public class Codec {
 
-   public String serialize(TreeNode root) {
-    StringBuilder sb = new StringBuilder();
-    dfs(root, sb);
-    return sb.toString();
-}
-private void dfs(TreeNode root, StringBuilder sb) {
-    if (root == null) {
-        return;
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfs(root,sb);
+        return sb.toString();
     }
-    sb.append(root.val + ",");
-    dfs(root.left, sb);
-    dfs(root.right, sb);
-    return;
-}
+    public void dfs(TreeNode root,StringBuilder sb)
+    {
+        if(root==null)
+            return;
+        sb.append(root.val+",");
+        dfs(root.left,sb);
+        dfs(root.right,sb);
+    }
 
     // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-    String[] arr = data.split(",");
-    TreeNode root = null;
-    for (String s : arr) {
-        if (s.length() > 0) {
-            root = buildBST(root, Integer.parseInt(s));
-        }
-    }
-    return root;
-}
-
-    public TreeNode buildBST(TreeNode root, int v) {
-        if (root == null) return new TreeNode(v);
-        if (v < root.val) {
-            root.left = buildBST(root.left, v);
-        } else {
-            root.right = buildBST(root.right, v);
+    public TreeNode deserialize(String data) 
+    {
+        String[] str = data.split(",");
+        TreeNode root = null;
+        for(String s:str)
+        {
+            if(s.length()>0)
+            {
+                root = insert(root,Integer.parseInt(s));
+            }
         }
         return root;
-  }
+    }
+    public TreeNode insert(TreeNode root,int v)
+    {
+        if(root==null)
+            return new TreeNode(v);
+        if(v<root.val)
+            root.left = insert(root.left,v);
+        if(v>root.val)
+            root.right = insert(root.right,v);
+        return root;
+    }
 }
+
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
 // Codec deser = new Codec();
