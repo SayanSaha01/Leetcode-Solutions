2f723badd1f3c5fb1f595bae37fa3aab686d9840
Sayan Saha <sahensha1600@gmail.com>
2022-12-19 00:33:13 +0530

    Time: 5 ms (70.00%), Space: 39.2 MB (100.00%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -11,13 +11,16 @@ class Solution {
         int sum=0;
         for(int i=2;i<=n;i++)
         {
-            while(n%i==0)
+            while(n%i==0)    //suppose number is 8 so because we are using while loop 2 will keep on dividing 8 unless and untill we reach a number which is nt divisible by 2 then only we will break out of the while loop.
             {
                 sum+=i;
                 n=n/i;
             }
         }
         return isprime(sum)? sum : sum_of_prime_factors(sum);
+        //if the sum of prime factors obtained is prime then no more prime factors of it is possible  other than 1 and the number itself, then in such cases we return the sum of the prime factors of the original number 
+        
+// and if the sum of the prime factors of the numbers isnt prime then we pass the sum recursively  
     }
     public boolean isprime(int sum)
     {
