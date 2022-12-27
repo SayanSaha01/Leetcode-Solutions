374baa526f0f5f0eda6dede6773a09330004e51c
Sayan Saha <sahensha1600@gmail.com>
2022-12-27 19:16:45 +0530

    Time: 2 ms (92.08%), Space: 42.7 MB (92.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,7 +3,7 @@ class Solution {
         int max_reach_idx=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(i > max_reach_idx)
+            if(i > max_reach_idx)  // if at any instance the current index becomes greater than the max_reachable index then we cannot reach the end as max reachable index falls short and cannot move further
                 return false;
             max_reach_idx = Math.max(i+nums[i],max_reach_idx);
             
