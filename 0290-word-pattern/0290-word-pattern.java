966d93bb8667913e3e6f5b35cad699fdfcc94caa
Sayan Saha <sahensha1600@gmail.com>
2023-01-01 11:27:45 +0530

    Time: 1 ms (95.36%), Space: 40 MB (90.03%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character,String> map = new HashMap<Character,String>();
+        String[] str = s.split(" ");
+        if(pattern.length()!=str.length)
+            return false;
+        for(int i=0;i<pattern.length();i++)
+        {
+            if(!map.containsKey(pattern.charAt(i)))
+            {
+                if(map.containsValue(str[i]))
+                    return false;
+                map.put(pattern.charAt(i),str[i]);
+            }
+            else if(map.containsKey(pattern.charAt(i)))
+            {
+                if(!map.get(pattern.charAt(i)).equals(str[i]))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
