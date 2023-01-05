c004010b613155b33a018e558a083bb201351efd
Sayan Saha <sahensha1600@gmail.com>
2023-01-05 16:55:12 +0530

    Time: 2 ms (99.86%), Space: 47 MB (73.17%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/lexicographical-numbers/discuss/86269/Simple-Java-DFS-Solution-(beats-85-12-lines)
 class Solution {
     List<Integer> list = new ArrayList<Integer>();
     public List<Integer> lexicalOrder(int n) {
