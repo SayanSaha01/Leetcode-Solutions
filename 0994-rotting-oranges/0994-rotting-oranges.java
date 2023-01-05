d25419395c3bd891eb9f9861274021a15ab2ce28
Sayan Saha <sahensha1600@gmail.com>
2023-01-05 21:24:16 +0530

    Time: 3 ms (61.87%), Space: 43.4 MB (15.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,66 +1,49 @@
-class Pair{
-    int row;
-    int col;
-    int time;
-    
-    Pair(int row,int col,int time){
-        this.row=row;
-        this.col=col;
-        this.time=time;
-    }
-}
-
 class Solution {
-    int dir[][] = {{-1,0},{1,0},{0,1},{0,-1}};
-
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int count=0,max=0;
-        
-        Queue<Pair> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[row][col];
-        
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==2){
-                    queue.add(new Pair(i,j,0));
+        if(grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+        //Put the position of all rotten oranges in queue
+        //count the number of fresh oranges
+        for(int i = 0 ; i < rows ; i++) {
+            for(int j = 0 ; j < cols ; j++) {
+                if(grid[i][j] == 2) {
+                    queue.offer(new int[]{i , j});
+                }
+                else if(grid[i][j] == 1) {
+                    count_fresh++;
                 }
             }
         }
-        
-        while(!queue.isEmpty())
-        {
-            Pair p = queue.remove();
-            visited[p.row][p.col]=true;
-            
-            for(int dr[] : dir)
-            {
-                int i = p.row+dr[0];
-                int j = p.col+dr[1];
-                
-                if(isValidDir(i,j,grid) && grid[i][j]==1 && !visited[i][j])
-                {
-                    grid[i][j]=2;
-                    queue.add(new Pair(i,j,p.time+1));
+        //if count of fresh oranges is zero --> return 0 
+        if(count_fresh == 0) return 0;
+        int count = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        //bfs starting from initially rotten oranges
+        while(!queue.isEmpty()&&count_fresh>0) {
+            ++count;
+            int size = queue.size();
+            for(int i = 0 ; i < size ; i++) {
+                int[] point = queue.poll();
+                for(int dir[] : dirs) {
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+                    //if x or y is out of bound
+                    //or the orange at (x , y) is already rotten
+                    //or the cell at (x , y) is empty
+                        //we do nothing
+                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
+                    //mark the orange at (x , y) as rotten
+                    grid[x][y] = 2;
+                    //put the new rotten orange at (x , y) in queue
+                    queue.offer(new int[]{x , y});
+                    //decrease the count of fresh oranges by 1
+                    count_fresh--;
                 }
             }
-            max=Math.max(max,p.time);
         }
-        for(int rows[] : grid){
-            for(int cols : rows){
-                if(cols==1) count++;
-            }
-        }
-        return count>0 ? -1 : max;
-    }
-    
-    private boolean isValidDir(int i,int j,int grid[][]){
-        if(i<0 || i>=grid.length||
-           j<0 || j>=grid[0].length){
-            return false;
-        }
-        
-        return true;
+        return count_fresh == 0 ? count : -1;
     }
 }
