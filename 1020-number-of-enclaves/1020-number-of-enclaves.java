5026339f3ea4692a14ef50cfc99a212eac18b80d
Sayan Saha <sahensha1600@gmail.com>
2023-01-05 16:02:54 +0530

    Time: 12 ms (67.61%), Space: 75.4 MB (32.90%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,39 +1,37 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int result = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<grid.length;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<grid[0].length;j++)
             {
-                if(i==0 || j==0 || i==n-1 || j==m-1)
+                if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1)
                 {
                     dfs(grid,i,j);
                 }
             }
         }
+        int count=0;
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    result++;
+                    count++;
                 }
             }
         }
-        return result;
+        return count;
     }
     public void dfs(int[][] grid,int i,int j)
     {
-        if(i>=0 && i<=grid.length-1 && j>=0 && j<=grid[0].length-1 && grid[i][j]==1)
+        if(i>=0 && j>=0 && j<grid[0].length && i<grid.length && grid[i][j]==1)
         {
             grid[i][j]=0;
-            dfs(grid,i-1,j);
-            dfs(grid,i,j+1);
-            dfs(grid,i,j-1);
             dfs(grid,i+1,j);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j-1);
+            dfs(grid,i,j+1);
         }
     }
 }
