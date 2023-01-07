4efab283958bae47433a30cc566d2d4dd7434e2a
Sayan Saha <sahensha1600@gmail.com>
2023-01-07 12:15:23 +0530

    Time: 13 ms (85.36%), Space: 44.6 MB (89.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -7,13 +7,13 @@ class Solution {
         {
             for(int j=0;j<m;j++)
             {
-                if(mat[i][j]==0) //we need to find out,distance of nearest 0 but lets find out the distance from the cells marked 0
+                if(mat[i][j]==0) //we need to find out,distance of nearest 0 from each cell but lets do the reverse and find out the min distance from the cells marked 0
                 {
                     queue.add(new int[]{i,j});
                 }
                 else
                 {
-                    mat[i][j]=-1;
+                    mat[i][j]=-1;     // we are marking every 1 with -1 to get any idea which cells to visit during bfs
                 }
             }
         }
@@ -30,7 +30,7 @@ class Solution {
                 {
                     int row = d[0]+node[0];
                     int col = d[1]+node[1];
-                    if(row>=0 && col>=0 && row<mat.length && col<mat[0].length && mat[row][col]==-1)
+                    if(row>=0 && col>=0 && row<mat.length && col<mat[0].length && mat[row][col]==-1)     //we will visit only those nearby cells having -1 that we earlier marked to find nearest distance from cells marked 0, once we allocate them nearest value we cant revisit them again as it doesnt satisfy the condition as mat[row][col] will have values like 1,2,3 and we specifically visit those rows not visited and having mat[row][col]=-1. why does this method work, we move from cells with 0 and consecutively updating the neighbours with the least val we push it into the queue to find the next least values for the neighbouring cells having val -1
                     {
                         mat[row][col]=nearest;
                         queue.add(new int[]{row,col});
