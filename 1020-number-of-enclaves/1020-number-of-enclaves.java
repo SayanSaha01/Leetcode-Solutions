c5c09245025d9c90b89d19250a823cf65b604cfd
Sayan Saha <sahensha1600@gmail.com>
2023-01-07 22:15:05 +0530

    Time: 7 ms (99.25%), Space: 54.5 MB (85.32%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,24 +1,30 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        for(int i=0;i<grid.length;i++)
+        int row = grid.length;
+        int col = grid[0].length;
+        //checking for 1s in first and last row of matrix
+        for(int i=0;i<col;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
-            {
-                if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1)
-                {
-                    dfs(grid,i,j);
-                }
-            }
+            if(grid[0][i]==1)
+                dfs(grid,0,i);
+            if(grid[row-1][i]==1)
+                dfs(grid,row-1,i);
+        }
+        //cehcking for 1's in first and last col of matrix
+        for(int i=0;i<row;i++)
+        {
+            if(grid[i][0]==1)
+                dfs(grid,i,0);
+            if(grid[i][col-1]==1)
+                dfs(grid,i,col-1);
         }
         int count=0;
-        for(int i=0;i<grid.length;i++)
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
+            for(int j=0;j<col;j++)
             {
                 if(grid[i][j]==1)
-                {
                     count++;
-                }
             }
         }
         return count;
@@ -27,11 +33,11 @@ class Solution {
     {
         if(i>=0 && j>=0 && j<grid[0].length && i<grid.length && grid[i][j]==1)
         {
-            grid[i][j]=0;
+            grid[i][j]=2;
             dfs(grid,i+1,j);
             dfs(grid,i-1,j);
-            dfs(grid,i,j-1);
             dfs(grid,i,j+1);
+            dfs(grid,i,j-1);
         }
     }
 }
