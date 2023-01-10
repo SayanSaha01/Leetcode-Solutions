8b11554b1cb005f603536ba5bf824a084382fc9f
Sayan Saha <sahensha1600@gmail.com>
2023-01-10 12:33:39 +0530

    Time: 73 ms (81.07%), Space: 60.8 MB (79.60%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+		// graph creation
+        for(int i=0;i<n;i++){
+            a.add(new ArrayList<Integer>());
+        }
+        
+        for(int i=0;i<manager.length;i++){
+            if(manager[i]>=0)
+                a.get(manager[i]).add(i);
+        }
+        return dfs(headID,informTime); 
+    }
+    private int dfs(int node,int[] informTime){
+        
+        int max = 0;
+        for(int i:a.get(node)){
+            max = Math.max(max,dfs(i,informTime)+informTime[node]);
+        }
+        return max;
+    }
+}
