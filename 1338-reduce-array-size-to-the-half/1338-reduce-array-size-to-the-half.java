7844aa29e81410bfde1ec7d3db3f3c890e049ec6
Sayan Saha <sahensha1600@gmail.com>
2023-01-11 11:03:09 +0530

    Time: 41 ms (80.80%), Space: 50.3 MB (99.33%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        HashMap<Integer,Integer> map= new HashMap<Integer,Integer>();
-        for(int i : arr) 
-            map.put(i , map.getOrDefault(i,0)+1);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); 
-
-        for(int i : map.keySet()) 
-            pq.add(map.get(i));
-
-        int count = 0;
-        int itemRemoved=arr.length;
-
-        while(!pq.isEmpty()){
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i:arr)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int i:map.values())
+        {
+            pq.add(i);
+        }
+        int count=0;
+        int size=arr.length;
+        while(!pq.isEmpty())
+        {
+            int node=pq.poll();
             count++;
-            itemRemoved-=pq.remove();
-            if(itemRemoved<=arr.length/2) 
+            size-=node;          //since it is mentioned a least half of the integers of the arrays are to be removed there is no harm if more than half of the integers are removed but after the immediate step we must stop hence the break cndition
+            if(size<=arr.length/2)
                 break;
         }
         return count;
-
     }
 }
