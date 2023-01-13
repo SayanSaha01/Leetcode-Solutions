e6816f9012fdee05379bfc1449b9c752998c1e7c
Sayan Saha <sahensha1600@gmail.com>
2023-01-13 10:48:13 +0530

    Time: 81 ms (90.11%), Space: 105.4 MB (96.70%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -1,46 +1,44 @@
 class Solution {
-  int [] result;
-  List<List<Integer>> adj;
-  boolean[] visited;
-//This function takes in the number of nodes in a tree, the edges that connect the nodes and the labels of the nodes
-   public int[] countSubTrees(int n, int[][] edges, String labels) {
-//Creating an adjacency list to store the edges
-     adj = new ArrayList<>(n);
-     result = new int[n];
-    //initializing the adjacency list
-    for(int i = 0; i < n; i++){
-        adj.add(new ArrayList<>());
+    List<List<Integer>> list;
+    int[] result;
+    boolean[] visited;
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        list = new ArrayList<>(n);
+        result = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            list.add(new ArrayList<>());
+        }
+        for(int [] edge:edges)
+        {
+            list.get(edge[0]).add(edge[1]);
+            list.get(edge[1]).add(edge[0]);
+        }
+        visited = new boolean[n];
+        dfs(0,labels);
+        return result;
     }
-    //populating the adjacency list with the edges
-    for(int[] edge : edges){
-        adj.get(edge[0]).add(edge[1]);
-        adj.get(edge[1]).add(edge[0]);
-    }
-    //keep track of visited nodes
-     visited = new boolean[n];
-    //calling the dfs function to count the subtrees starting from the root node
-    dfs(0,labels);
-    return result;
-}
-//dfs function to traverse the tree and count the subtrees
-int[] dfs(int node, String labels){
-    visited[node] = true;
-    int[] count = new int[26];
-    //visiting all the neighbours of the current node
-    for(int nbr : adj.get(node)){
-        if(!visited[nbr]){
-            int [] adjCount = dfs(nbr, labels);
-            //updating the count array with the count of the subtrees of the neighbours
-            for(int i = 0 ; i < 26; i++){
-                count[i] += adjCount[i];
+    public int[] dfs(int node, String labels)
+    {
+        visited[node]=true;
+        int[] label = new int[26];   // to keep track of all the labels in its subtree or children nodes
+        for(int it:list.get(node))
+        {
+            if(!visited[it])
+            {
+                int[] child_label = dfs(it,labels);
+                //updating the labels with the occurence of child_label in each of its subtree from the node
+                for(int i =0;i<26;i++)
+                {
+                    label[i]+=child_label[i];
+                }
             }
         }
+        //updating the label array with the label present at the current node, we were adding the child_nodes previously in the label
+        char ch = labels.charAt(node);
+        label[ch-'a']++;
+        //storing the count of the subtrees of the current node, label array is being initialised every time we go the children of nodes
+        result[node]=label[ch-'a'];
+        return label;
     }
-    //incrementing the count of the current node label
-    char ch = labels.charAt(node);
-    count[ch-'a']++;
-    //storing the count of the subtrees of the current node
-    result[node] = count[ch-'a'];
-    return count;
-  }
 }
