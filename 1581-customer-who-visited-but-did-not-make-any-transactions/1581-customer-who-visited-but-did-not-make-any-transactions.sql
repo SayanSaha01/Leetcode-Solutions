2d67a46079e5fa287fff868c58a2f9cdce67d878
Sayan Saha <sahensha1600@gmail.com>
2023-01-16 09:54:22 +0530

    Time: 1894 ms (32.91%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,1 +1,7 @@
-select customer_id, count(v.visit_id) as count_no_trans from Visits v left join Transactions t on v.visit_id=t.visit_id where t.visit_id is null group by customer_id;
+select customer_id, count(v.visit_id) as count_no_trans 
+from Visits v left join Transactions t 
+on v.visit_id=t.visit_id 
+where t.visit_id is null 
+group by customer_id;
+
+/*We can see that if someone makes a visit but doesn't make a transaction, the customer_id will be registered in the Visits table yet there will not be a transaction_id corresponding to that visit_id. So we can take advantage of the NULLs that result when performing a LEFT JOIN with Visits on the left joined with Transactions.*/
