97eae6246c4ce96e261382aede9285cb66f6f1e1
Sayan Saha <sahensha1600@gmail.com>
2023-01-16 19:06:12 +0530

    Time: 100 ms (72.97%), Space: 44 MB (82.02%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,43 +1,46 @@
 class Pair{
     String first;
-    int second;
-    Pair(String first,int second)
+    int count;
+    Pair(String first,int count)
     {
         this.first = first;
-        this.second = second;
+        this.count = count;
     }
 }
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(beginWord,1));
-        HashSet<String> set = new HashSet<>(); 
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) 
+    {
+        HashSet<String> set = new HashSet<>();
         for(int i=0;i<wordList.size();i++)
         {
             set.add(wordList.get(i));
         }
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(beginWord,1));
         set.remove(beginWord);
-        while(!q.isEmpty())
+        while(!queue.isEmpty())
         {
-            String word = q.peek().first;
-            int step = q.peek().second;
-            q.remove();
+            String word = queue.peek().first;
+            int step = queue.peek().count;
+            queue.poll();
             
             if(word.equals(endWord)==true)
                 return step;
-            for (int i = 0; i < word.length(); i++) {
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    char replacedCharArray[] = word.toCharArray();
-                    replacedCharArray[i] = ch;
-                    String replacedWord = new String(replacedCharArray);
-
-                    // check if it exists in the set and push it in the queue.
-                    if (set.contains(replacedWord) == true) {
-                        set.remove(replacedWord);
-                        q.add(new Pair(replacedWord, step + 1));
-                    }
+            
+            for(int i=0;i<word.length();i++)
+            {
+                for(char ch='a';ch<='z';ch++)
+                {
+                    char[] arr = word.toCharArray();
+                    arr[i]=ch;
+                    String newstr = new String(arr);
+                    
+                    if(set.contains(newstr))
+                    {
+                        set.remove(newstr);
+                        queue.add(new Pair(newstr,step+1));
+                    }    
                 }
-
             }
         }
         return 0;
