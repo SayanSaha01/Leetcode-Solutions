0825525dd255c99fc91f41590af00fdd3877baed
Sayan Saha <sahensha1600@gmail.com>
2023-02-08 10:14:16 +0530

    Time: 2 ms (16.47%), Space: 41.6 MB (94.00%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,39 @@
+/*class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int op1 = help(0,cost);
+        int op2 = help(1,cost);
+        return Math.min(op1,op2);
+    }
+    public int help(int curr_stair,int[] cost)
+    {
+        if(curr_stair>=cost.length)
+            return 0;
+        int total = cost[curr_stair];
+        int op1 = help(curr_stair+1,cost);
+        int op2 = help(curr_stair+2,cost);
+        total+=Math.min(op1,op2);
+        return total;
+    }
+}*/
+
+class Solution {
+    int[] dp = new int[1001];
+    public int minCostClimbingStairs(int[] cost) {
+        Arrays.fill(dp,-1);
+        int op1 = help(0,cost);
+        int op2 = help(1,cost);
+        return Math.min(op1,op2);
+    }
+    public int help(int curr_stair,int[] cost)
+    {
+        if(curr_stair>=cost.length)
+            return 0;
+        if(dp[curr_stair]!=-1)
+            return dp[curr_stair];
+        int total = cost[curr_stair];
+        int op1 = help(curr_stair+1,cost);
+        int op2 = help(curr_stair+2,cost);
+        total+=Math.min(op1,op2);
+        return dp[curr_stair]=total;
+    }
+}
