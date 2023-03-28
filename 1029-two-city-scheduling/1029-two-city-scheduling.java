9571ce2e0dc48df64cf1a4d4b917a9ceca175dec
Sayan Saha <sahensha1600@gmail.com>
2023-03-28 19:40:56 +0530

    Time: 2 ms (64.36%), Space: 40.6 MB (63.19%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,6 +1,6 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs,(a,b)->(a[0]-a[1])-(b[0]-b[1]));
+        Arrays.sort(costs,(a,b)->(a[0]-a[1])-(b[0]-b[1]));   //basis of sorting on the basis of cost difference, if we get negative then we understand the first value is very less and the second value of array is big and when the cost difference is +ve we understand that the first value is big and the second value is less hence we assign the first n values which have negative difference to city a and the next n values which have positive difference to city b .
         int sum=0;
         for(int i=0;i<costs.length/2;i++)
         {
