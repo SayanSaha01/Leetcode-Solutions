f550a55ff6a6407292efa0ba9b0398e406b055fe
Sayan Saha <sahensha1600@gmail.com>
2023-04-30 11:07:34 +0530

    Time: 33 ms (100.00%), Space: 65.9 MB (100.00%) - LeetHub

---
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        HashMap<Integer,int[]> map = new HashMap<>();
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+                map.put(mat[i][j],new int[]{i,j});
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            int[] loc = map.get(arr[i]);
+            int x = loc[0];
+            int y = loc[1];
+            row[x]++;
+            col[y]++;
+            if(row[x]==n || col[y]==m)
+                return i;
+        }
+        return arr.length-1;
+    }
+}
