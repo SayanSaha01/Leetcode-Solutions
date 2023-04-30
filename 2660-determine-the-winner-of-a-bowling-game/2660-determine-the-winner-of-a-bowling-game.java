93a4887ef8bf86eb55c3ac8b7d9737464edde5aa
Sayan Saha <sahensha1600@gmail.com>
2023-04-30 09:54:18 +0530

    Time: 2 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

---
+++ 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int isWinner(int[] player1, int[] player2) {
+        int sum1=0,sum2=0;
+        for(int i=0;i<player1.length;i++)
+        {
+            if(i==1)
+            {
+                if(player1[i-1]==10)
+                    sum1+=2*player1[i];
+                else
+                    sum1+=player1[i];
+            }
+            else if(i>1)
+            {
+                if(player1[i-1]==10 || player1[i-2]==10)
+                    sum1+=2*player1[i];
+                else
+                    sum1+=player1[i];
+            }
+            else
+                sum1+=player1[i];
+        }
+        for(int i=0;i<player2.length;i++)
+        {
+            if(i==1)
+            {
+                if(player2[i-1]==10)
+                    sum2+=2*player2[i];
+                else
+                    sum2+=player2[i];
+            }
+            else if(i>1)
+            {
+                if(player2[i-1]==10 || player2[i-2]==10)
+                    sum2+=2*player2[i];
+                else
+                    sum2+=player2[i];
+            }
+            else
+                sum2+=player2[i];
+        }
+        if(sum1>sum2)
+            return 1;
+        else if(sum2>sum1)
+            return 2;
+        else
+            return 0;
+    }
+}
