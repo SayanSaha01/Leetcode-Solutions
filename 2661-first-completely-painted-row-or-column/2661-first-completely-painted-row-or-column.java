ac8893a706ff1ce69c3e23d3fd6a01da6eb12796
Sayan Saha <sahensha1600@gmail.com>
2023-05-02 20:21:59 +0530

    Time: 31 ms (74.86%), Space: 65.9 MB (63.12%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -20,6 +20,7 @@ class Solution {
             row[x]++;
             col[y]++;
             if(row[x]==n || col[y]==m)
+                //why we are checking row with n where n = column length this is because we are trying to find if occurence of a particular values covers all the columns of a row
                 return i;
         }
         return arr.length-1;
