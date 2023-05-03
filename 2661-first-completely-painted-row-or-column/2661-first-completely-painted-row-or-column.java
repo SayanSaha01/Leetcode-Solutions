c363cfc14f6bf49beeb318bf24d62dfbd9a9fc31
Sayan Saha <sahensha1600@gmail.com>
2023-05-03 19:22:41 +0530

    Time: 31 ms (74.05%), Space: 66.4 MB (42.41%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -1,10 +1,8 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        HashMap<Integer,int[]> map = new HashMap<>();
         int m = mat.length;
         int n = mat[0].length;
-        int[] row = new int[m];
-        int[] col = new int[n];
+        HashMap<Integer,int[]> map = new HashMap<>();
         for(int i=0;i<mat.length;i++)
         {
             for(int j=0;j<mat[0].length;j++)
@@ -12,6 +10,10 @@ class Solution {
                 map.put(mat[i][j],new int[]{i,j});
             }
         }
+        
+        int[] row = new int[m];
+        int[] col = new int[n];
+        
         for(int i=0;i<arr.length;i++)
         {
             int[] loc = map.get(arr[i]);
@@ -20,7 +22,6 @@ class Solution {
             row[x]++;
             col[y]++;
             if(row[x]==n || col[y]==m)
-                //why we are checking row with n where n = column length this is because we are trying to find if occurence of a particular values covers all the columns of a row
                 return i;
         }
         return arr.length-1;
