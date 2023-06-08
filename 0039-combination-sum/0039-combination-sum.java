d39a3fcc2eacbeab1b4359a1b1b2cee49c10a2ae
Sayan Saha <sahensha1600@gmail.com>
2023-06-08 19:48:56 +0530

    Time: 4 ms (24.11%), Space: 44.2 MB (11.59%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> inner = new ArrayList<>();
-        combination(list,inner,candidates,target,0);
-        return list;
-    }
-    public void combination(List<List<Integer>> list,List<Integer> inner,int[] candidates,int target,int idx)
-    {
-        if(idx==candidates.length)
-        {
-            if(target==0)
-                list.add(new ArrayList<>(inner));
-                return;
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(candidates);
+    backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+    return list;
+}
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+        if (remain < 0) return; /** no solution */
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for (int i = start; i < cand.length; i++) { 
+                tempList.add(cand[i]);
+                backtrack(list, tempList, cand, remain-cand[i], i);
+                tempList.remove(tempList.size()-1);
+            } 
         }
-        
-        if(candidates[idx]<=target)
-        {
-            inner.add(candidates[idx]);
-            combination(list,inner,candidates,target-candidates[idx],idx);;
-            inner.remove(inner.size()-1);
-        }
-        combination(list,inner,candidates,target,idx+1);
+
     }
 }
