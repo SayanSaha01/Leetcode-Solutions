04fc62b1eaa87f52d5933ee1531d15d6f8a276dd
Sayan Saha <sahensha1600@gmail.com>
2023-06-08 09:38:26 +0530

    Time: 3 ms (20.71%), Space: 44.1 MB (9.35%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> inner = new ArrayList<>();
+        uniquesubset(set,inner,nums,0);
+        
+        List<List<Integer>> convertedList = new ArrayList<>();
+        for (List<Integer> l : set) {
+            ArrayList<Integer> arrayList = new ArrayList<>(l);
+            convertedList.add(arrayList);
+        }
+        
+        return convertedList;
+    }
+    public void uniquesubset(Set<List<Integer>> set,List<Integer> inner,int[] nums,int idx)
+    {
+        if(idx>=nums.length)
+        {
+            set.add(new ArrayList<>(inner));
+            return;
+        }
+        
+        //pick
+        inner.add(nums[idx]);
+        uniquesubset(set,inner,nums,idx+1);
+        
+        //not pick
+        inner.remove(inner.size()-1);
+        uniquesubset(set,inner,nums,idx+1);
+
+    }
+}
