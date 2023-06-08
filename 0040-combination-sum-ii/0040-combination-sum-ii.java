8590ea1aba0ce21352f7d90a36dce4654762c90d
Sayan Saha <sahensha1600@gmail.com>
2023-06-08 19:54:00 +0530

    Time: 5 ms (42.87%), Space: 42.3 MB (94.49%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> arraylist = new ArrayList<>();
-        List<Integer> inner = new ArrayList<>();
-        Arrays.sort(candidates);
-        unique_combination(arraylist,inner,candidates,target,0);
+   List<List<Integer>> list = new LinkedList<List<Integer>>();
+   Arrays.sort(candidates);
+   backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+   return list;
+}
 
-        return arraylist;
-    }
-    public void unique_combination(List<List<Integer>> set,List<Integer> inner,int[] candidates,int target,int start)
-    {
-        if(target == 0) 
-            set.add(new ArrayList<>(inner));
-        for (int i = start; i < candidates.length; i++) 
-        {
-             if(i > start && candidates[i] == candidates[i-1]) 
-                 continue; /** skip duplicates */
-             if(candidates[i]>target)
-                 break;
-             inner.add(candidates[i]);
-             unique_combination(set, inner, candidates, target - candidates[i], i+1);
-             inner.remove(inner.size() - 1);
-        }
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
+
+       if(remain < 0) return; /** no solution */
+       else if(remain == 0) list.add(new ArrayList<>(tempList));
+       else{
+          for (int i = start; i < cand.length; i++) {
+             if(i > start && cand[i] == cand[i-1]) continue; /** skip duplicates */
+             tempList.add(cand[i]);
+             backtrack(list, tempList, cand, remain - cand[i], i+1);
+             tempList.remove(tempList.size() - 1);
+          }
+       }
     }
 }
