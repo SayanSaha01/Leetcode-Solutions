9ae409a731832fe3df9dda74d552e24949df57b0
Sayan Saha <sahensha1600@gmail.com>
2023-06-08 14:13:11 +0530

    Time: 2 ms (99.43%), Space: 42.4 MB (94.49%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,33 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> arraylist = new ArrayList<>();
         List<Integer> inner = new ArrayList<>();
         Arrays.sort(candidates);
-        unique_combination(set,inner,candidates,target,0);
-        
-        List<List<Integer>> arraylist = new ArrayList<>();
-        for(List<Integer> l :set)
-        {
-            arraylist.add(l);
-        }
+        unique_combination(arraylist,inner,candidates,target,0);
+
         return arraylist;
     }
-    public void unique_combination(Set<List<Integer>> set,List<Integer> inner,int[] candidates,int target,int start)
+    public void unique_combination(List<List<Integer>> set,List<Integer> inner,int[] candidates,int target,int start)
     {
-        if(target < 0) 
-            return; /** no solution */
-        else if(target == 0) 
+        if(target == 0) 
             set.add(new ArrayList<>(inner));
-        else
+        for (int i = start; i < candidates.length; i++) 
         {
-          for (int i = start; i < candidates.length; i++) 
-          {
              if(i > start && candidates[i] == candidates[i-1]) 
                  continue; /** skip duplicates */
+             if(candidates[i]>target)
+                 break;
              inner.add(candidates[i]);
              unique_combination(set, inner, candidates, target - candidates[i], i+1);
              inner.remove(inner.size() - 1);
-          }
         }
     }
 }
