58c06078aa5ab9a8d203e30f6201a6889c266fb6
Sayan Saha <sahensha1600@gmail.com>
2023-06-09 10:07:30 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (52.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,36 +1,40 @@
-/*class Solution {
+/*
+class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        return recursion(0,nums);
+        return max_sum(nums,0);
     }
-    public int recursion(int idx,int[] nums)
+    public int max_sum(int[] nums,int idx)
     {
-        if(idx==nums.length-1)    //if there is only 1 element
+        if(idx==nums.length-1)
             return nums[idx];
         if(idx>nums.length-1)
             return 0;
-        int incl = recursion(idx+2,nums)+nums[idx];
-        int excl = recursion(idx+1,nums)+0;
-        return Math.max(incl,excl);
+        int include = max_sum(nums,idx+2) + nums[idx];
+        int exclude = max_sum(nums,idx+1) + 0;
+        return Math.max(include,exclude);
     }
-}*/
+}
+*/
+
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return recursion(0,nums,dp);
+        return max_sum(nums,0,dp);
     }
-    public int recursion(int idx,int[] nums,int[] dp)
+    public int max_sum(int[] nums,int idx,int[] dp)
     {
-        if(idx==nums.length-1)    //if there is only 1 element
+        if(idx==nums.length-1)
             return nums[idx];
         if(idx>nums.length-1)
             return 0;
         if(dp[idx]!=-1)
             return dp[idx];
-        int incl = recursion(idx+2,nums,dp)+nums[idx];
-        int excl = recursion(idx+1,nums,dp)+0;
-        return dp[idx]=Math.max(incl,excl);
+        int include = max_sum(nums,idx+2,dp) + nums[idx];
+        int exclude = max_sum(nums,idx+1,dp) + 0;
+        return dp[idx] = Math.max(include,exclude);
     }
 }
+
