667fc653c4f01741e6de3b2802000d05e348f5bb
Sayan Saha <sahensha1600@gmail.com>
2023-06-14 10:26:59 +0530

    Time: 6 ms (27.58%), Space: 46.6 MB (66.25%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,8 +3,19 @@ class Solution {
         int[] arr = new int[n+1];
         for(int i=0;i<=n;i++)
         {
-            arr[i]=Integer.bitCount(i);
+            arr[i]=countones(i);
         }
         return arr;
+    }
+    public int countones(int num)
+    {
+        int n1=0;
+        while(num!=0)
+        {
+            if((num&1)==1)
+                n1++;
+            num=num>>1;
+        }
+        return n1;
     }
 }
