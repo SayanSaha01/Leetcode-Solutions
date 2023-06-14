9b40d21b3683fd87ddb44570d56f2ef0eae66582
Sayan Saha <sahensha1600@gmail.com>
2023-06-14 09:16:43 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (88.37%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,13 +1,10 @@
 class Solution {
     public int xorOperation(int n, int start) {
         int[] arr = new int[n];
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=2*i+start;
-        }
         int xor=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arr.length;i++)
         {
+            arr[i]=start+2*i;
             xor^=arr[i];
         }
         return xor;
