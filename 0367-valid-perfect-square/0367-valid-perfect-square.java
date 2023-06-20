fb817c290e7f55e1e223a000212a2fe3c1d2cd8f
Sayan Saha <sahensha1600@gmail.com>
2023-06-20 22:11:05 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (80.44%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int a=1;
+        while(num>0)
+        {
+            num=num-a;
+            a=a+2;
+        }
+        return num==0;
+    }
+}
