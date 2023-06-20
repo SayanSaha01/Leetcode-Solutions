65f4cc2293f058b7e28b75c04d0626c4cbaad70f
Sayan Saha <sahensha1600@gmail.com>
2023-06-20 21:45:08 +0530

    Time: 77 ms (8.32%), Space: 53.7 MB (5.47%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -5,9 +5,6 @@ class Solution {
         for(int i=0;i<=Math.sqrt(c);i++)
         {
             set.add(i*i);
-        }
-        for(int i=0;i<=Math.sqrt(c);i++)
-        {
             if(set.contains(c-i*i))
                 return true;
         }
