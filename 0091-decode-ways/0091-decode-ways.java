fd260f9a5e5806a2f62c4496346ff8d44028b4cb
Sayan Saha <sahensha1600@gmail.com>
2023-06-21 18:56:28 +0530

    Time: 1 ms (95.96%), Space: 40.8 MB (81.07%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,16 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
         int n=s.length();
-        Integer[] mem=new Integer[n];
+        int[] mem=new int[n];
+        Arrays.fill(mem,-1);
         return s.length()==0?0:numDecodings(0,s,mem);      
     }
-    private int numDecodings(int p, String s, Integer[] mem) {
+    private int numDecodings(int p, String s, int[] mem) {
         int n=s.length();
         if(p==n) 
             return 1;
         if(s.charAt(p)=='0') 
             return 0;
-        if(mem[p]!=null) return mem[p];
+        if(mem[p]!=-1) 
+            return mem[p];
         int res=numDecodings(p+1,s,mem);
         if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7')) 
 			res+=numDecodings(p+2,s,mem);
