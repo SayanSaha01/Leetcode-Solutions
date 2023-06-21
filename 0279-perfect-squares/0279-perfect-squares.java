bf6a745ab9631a8201934b7abfaa17c74a4d502e
Sayan Saha <sahensha1600@gmail.com>
2023-06-21 16:53:14 +0530

    Time: 80 ms (31.10%), Space: 43.1 MB (23.86%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,35 +1,37 @@
-/*class Solution {
+/*
+class Solution {
+    int min = Integer.MAX_VALUE;
     public int numSquares(int n) {
-        return recursion(n);
+        return lps(n);
     }
-    public int recursion(int n)
+    private int lps(int n)
     {
         if(n==0)
             return 0;
-        int min = n;
         for(int i=1;i*i<=n;i++)
         {
-            min = Math.min(min,1+recursion(n-i*i));
+            min=Math.min(min,1+lps(n-i*i));
         }
         return min;
     }
-}*/
+}
+*/
 class Solution {
+    int min = Integer.MAX_VALUE;
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return recursion(n,dp);
+        return lps(n,dp);
     }
-    public int recursion(int n,int[] dp)
+    private int lps(int n,int[] dp)
     {
         if(n==0)
             return 0;
         if(dp[n]!=-1)
             return dp[n];
-        int min = n;
         for(int i=1;i*i<=n;i++)
         {
-            min = Math.min(min,1+recursion(n-i*i,dp));
+            min=Math.min(min,1+lps(n-i*i,dp));
         }
         return dp[n]=min;
     }
