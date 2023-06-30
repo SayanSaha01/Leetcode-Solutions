b4336234341315bcb73612df09ac5f96c620fb43
Sayan Saha <sahensha1600@gmail.com>
2023-06-30 21:36:03 +0530

    Time: 20 ms (15.04%), Space: 44 MB (18.66%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,6 @@
+import java.math.*;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        return ((new BigInteger(num1)).add(new BigInteger(num2))).toString();
+    }
+}
