8a9fb9fb26f2682969772eaeb3a18634b88ecac0
Sayan Saha <sahensha1600@gmail.com>
2023-07-03 15:31:13 +0530

    Time: 23 ms (67.31%), Space: 45.7 MB (19.92%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,27 +1,23 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
     List<List<Integer>> result = new ArrayList<>();            //used to store the result (all the possible combonations)
-    List<Integer> combinations = new ArrayList<>();            //use to store the combinations or we can say that subset
+    List<Integer> combinations = new ArrayList<>();         
     
-    solveitforMe(1,n,k,combinations,result);                   //Actual recursive logic
-    
+    solveitforMe(1,n,k,combinations,result);                
     return result;                
 }
 
-private void solveitforMe(int num, int n, int k, List<Integer> combinations, List<List<Integer>> result){
+private void solveitforMe(int num, int n, int k, List<Integer> inner, List<List<Integer>> result){
     
-    if(num>n+1)      
-        return;
-    
-    if(combinations.size() == k){             
-        result.add(new ArrayList(combinations));      
+    if(k==0) {
+        result.add(new ArrayList<Integer>(inner));
         return;
     }
-    
-    combinations.add(num);                             
-    solveitforMe(num+1,n,k,combinations,result);       
-    combinations.remove(combinations.size()-1);         
-    solveitforMe(num+1,n,k,combinations,result);        
-    
+    for(int i=num;i<=n;i++) {
+        inner.add(i);
+        solveitforMe(i+1, n, k-1,inner,result);
+        inner.remove(inner.size()-1);
+    }
+    return;
    }
 }
