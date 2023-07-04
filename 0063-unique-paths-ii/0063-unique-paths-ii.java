ccb20c9f96b1d9f0aa8f45933159a023aaff8e11
Sayan Saha <sahensha1600@gmail.com>
2023-07-05 00:13:23 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (44.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,4 +1,5 @@
-/*class Solution {
+/*
+class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -6,27 +7,28 @@
     }
     public int recursion(int i,int j,int[][] grid)
     {
+        if(i<0 || j<0)
+            return 0;
+        if(grid[i][j]==1)
+            return 0;
         if(i==0 && j==0)
             return 1;
-        if(i<0 || j<0 || grid[i][j]==1)
-            return 0;
-        int up = recursion(i-1,j,grid);
-        int left = recursion(i,j-1,grid);
-        return up+left;
+        return recursion(i-1,j,grid) + recursion(i,j-1,grid);
     }
-}*/
+}
+*/
+
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] d:dp)
-            Arrays.fill(d,-1);
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
         return recursion(m-1,n-1,obstacleGrid,dp);
     }
     public int recursion(int i,int j,int[][] grid,int[][] dp)
     {
-        
         if(i<0 || j<0)
             return 0;
         if(grid[i][j]==1)
@@ -35,9 +37,7 @@ class Solution {
             return 1;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int up = recursion(i-1,j,grid,dp);
-        int left = recursion(i,j-1,grid,dp);
-        dp[i][j]=up+left;
+        dp[i][j] = recursion(i-1,j,grid,dp) + recursion(i,j-1,grid,dp);
         return dp[i][j];
     }
 }
