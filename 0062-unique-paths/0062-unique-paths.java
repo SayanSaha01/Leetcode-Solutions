9292c431fa96c38938a39af5db91a0a707553dd9
Sayan Saha <sahensha1600@gmail.com>
2023-07-04 23:45:26 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (88.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,37 @@
 /*
 class Solution {
     public int uniquePaths(int m, int n) {
-        return count(m-1,n-1);
+        return recursion(m-1,n-1);
     }
-    public int count(int i,int j)
+    public int recursion(int i,int j)
     {
         if(i<0 || j<0)
             return 0;
         if(i==0 && j==0)
             return 1;
-        int up = count(i-1,j);
-        int left = count(i,j-1);
+        int up = recursion(i-1,j);
+        int left = recursion(i,j-1);
         return up+left;
     }
 }
 */
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
+        int[][] dp = new int[m][n];
         for(int[] d:dp)
             Arrays.fill(d,-1);
-        return count(m-1,n-1,dp);
+        return recursion(m-1,n-1,dp);
     }
-    public int count(int i,int j,int[][] dp)
+    public int recursion(int i,int j,int[][] dp)
     {
         if(i<0 || j<0)
             return 0;
-        if(i==0 && j==0)
-            return 1;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int up = count(i-1,j,dp);
-        int left = count(i,j-1,dp);
+        if(i==0 && j==0)
+            return 1;
+        int up = recursion(i-1,j,dp);
+        int left = recursion(i,j-1,dp);
         dp[i][j]=up+left;
         return dp[i][j];
     }
