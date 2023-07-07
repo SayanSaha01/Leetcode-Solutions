b050206926deeaaed4a8d2150c5620734cb1840a
Sayan Saha <sahensha1600@gmail.com>
2023-07-07 11:13:43 +0530

    Time: 1098 ms (5.03%), Space: 52.9 MB (83.39%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,26 +1,32 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        //initial position of left pointer
-        int l = 0;
-		//ma length of sequence so far
-        int max = 0;
-		//count of 0s we met so far
-        int count0 = 0;
-		
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                ++count0;
-            }
-			//we we met 2 0s - need to move left pointer unless number of 0s is 0 or 1
-            while(count0 > 1) {
-                if (nums[l] == 0)
-                    --count0;
-                ++l;
-            }
-			//update max length of sequence we've found so far
-            max = Math.max(max, i - l + 1);
+        int max=Integer.MIN_VALUE;
+        int cnt1=0;
+        int cnt0=0;
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+                cnt1++;
         }
-		//max will have length including 0, need to adjust it by 1
-        return max - 1;
+        if(cnt1==n)
+            return cnt1-1;
+        cnt1=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(cnt0>1)
+                    break;
+                if(nums[j]==1)
+                    cnt1++;
+                else if(nums[j]==0)
+                    cnt0++;
+            }
+            max=Math.max(max,cnt1);
+            cnt0=0;
+            cnt1=0;
+        }
+        return max;
     }
 }
