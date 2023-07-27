59cf335d802643b167ebab0f8b46a917d6986073
Sayan Saha <sahensha1600@gmail.com>
2023-07-27 11:44:00 +0530

    Time: 1 ms (99.77%), Space: 44.2 MB (40.15%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,4 +1,5 @@
-/*class Solution {
+/*
+class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -7,12 +8,12 @@
     public int recursion(int row,int col,int[][] grid)
     {
         if(row==0 && col==0)
-            return grid[row][col];
+            return grid[0][0];
         if(row==0)
             return grid[row][col]+recursion(row,col-1,grid);
         if(col==0)
-            return grid[row][col] + recursion(row-1,col,grid);
-        return grid[row][col]+Math.min(recursion(row,col-1,grid),recursion(row-1,col,grid));
+            return grid[row][col]+recursion(row-1,col,grid);
+        return grid[row][col]+Math.min(recursion(row-1,col,grid),recursion(row,col-1,grid));
     }
 }*/
 class Solution {
@@ -20,20 +21,20 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] arr:dp)
-            Arrays.fill(arr,-1);
+        for(int[] d:dp)
+            Arrays.fill(d,-1);
         return recursion(m-1,n-1,grid,dp);
     }
     public int recursion(int row,int col,int[][] grid,int[][] dp)
     {
         if(row==0 && col==0)
-            return grid[row][col];
+            return grid[0][0];
         if(dp[row][col]!=-1)
             return dp[row][col];
         if(row==0)
             return grid[row][col]+recursion(row,col-1,grid,dp);
         if(col==0)
-            return grid[row][col] + recursion(row-1,col,grid,dp);
-        return dp[row][col]= grid[row][col]+Math.min(recursion(row,col-1,grid,dp),recursion(row-1,col,grid,dp));
+            return grid[row][col]+recursion(row-1,col,grid,dp);
+        return dp[row][col] = grid[row][col]+Math.min(recursion(row-1,col,grid,dp),recursion(row,col-1,grid,dp));
     }
 }
