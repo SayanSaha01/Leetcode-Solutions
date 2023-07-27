33bdb902219d52bb4bab7d4479ef63fc8b7b38d6
Sayan Saha <sahensha1600@gmail.com>
2023-07-27 10:22:55 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (37.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,16 @@
 /*
 class Solution {
     public int uniquePaths(int m, int n) {
-        return recursion(m-1,n-1);
+        return paths(m-1,n-1);
     }
-    public int recursion(int i,int j)
+    public int paths(int i,int j)
     {
         if(i<0 || j<0)
             return 0;
         if(i==0 && j==0)
             return 1;
-        int up = recursion(i-1,j);
-        int left = recursion(i,j-1);
+        int up = paths(i-1,j);
+        int left = paths(i,j-1);
         return up+left;
     }
 }
@@ -26,13 +26,12 @@ class Solution {
     {
         if(i<0 || j<0)
             return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
         if(i==0 && j==0)
             return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
         int up = recursion(i-1,j,dp);
         int left = recursion(i,j-1,dp);
-        dp[i][j]=up+left;
-        return dp[i][j];
+        return dp[i][j]=up+left;
     }
 }
