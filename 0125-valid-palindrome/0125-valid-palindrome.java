09376d176f2e4da4425bfb49de90f8b830a1142b
Sayan Saha <sahensha1600@gmail.com>
2023-07-29 20:26:12 +0530

    Time: 24 ms (39.17%), Space: 44.2 MB (27.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","");   //output AmanaplanacanalPanama
-        s = s.toLowerCase();
-        char[] ch = s.toCharArray();
+        String str = s.replaceAll("[^a-zA-Z0-9]","");
+        System.out.print(str);
+        str = str.toLowerCase();
+        char[] arr = str.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i=(ch.length-1);i>=0;i--)
+        for(int i=arr.length-1;i>=0;i--)
         {
-            sb.append(ch[i]);
+            sb.append(arr[i]);
         }
         String rev = sb.toString();
-        return rev.equals(s);
+        return rev.equals(str);
     }
 }
