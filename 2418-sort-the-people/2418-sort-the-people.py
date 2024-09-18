717f0989d5e7a4d4bf7ee7b51a202166f11b5975
Sayan Saha <sahensha1600@gmail.com>
2024-09-18 22:44:02 +0530

    Time: 159 ms (8.58%), Space: 17.1 MB (64.73%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,8 +1,8 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        result = []
         sorted_heights = sorted(heights,reverse=True)
+        list2=[]
         for height in sorted_heights:
-            idx = heights.index(height)
-            result.append(names[idx])
-        return result
+            z = heights.index(height)
+            list2.append(names[z])
+        return list2
