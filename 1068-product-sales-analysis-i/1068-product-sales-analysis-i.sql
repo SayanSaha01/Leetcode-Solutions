ef3d22740258b342453aa700dabbe4ee8779bc92
Sayan Saha <sahensha1600@gmail.com>
2024-09-27 23:10:43 +0530

    Time: 1103 ms (82.69%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,3 @@
-select p.product_name,s.year,s.price 
-from Sales s join Product p
-on s.product_id=p.product_id
-order by s.price ;
+select p.product_name, s.year, s.price
+from Sales s inner join Product p
+on s.product_id = p.product_id
