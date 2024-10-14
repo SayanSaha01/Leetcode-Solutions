6744e3851273a7d0c7b79b5c3fe0af03b2cd7b8d
Sayan Saha <sahensha1600@gmail.com>
2024-10-14 20:49:33 +0530

    Time: 59 ms (60.65%), Space: 16.5 MB (86.20%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.py
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -1,13 +1,8 @@
-class Solution {
-    public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<words.length;i++)
-        {
-            if(words[i].contains(x+""))
-            {
-                list.add(i);
-            }
-        }
-        return list;
-    }
-}
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans=[]
+        for i,str in enumerate(words):
+            if x in str:
+                ans.append(i)
+        return ans
+            
