8832855198b3136be21d01f9d5688dea889da1e7
Sayan Saha <sahensha1600@gmail.com>
2024-12-01 23:16:29 +0530

    Time: 5140 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,6 @@
-with cte as(
+select person_name from (
     select *, sum(weight) over(order by Turn) as Total_Weight
     from Queue
-)
-select person_name from cte 
-where Total_Weight<=1000
-order by turn desc limit 1
+) x
+where x.Total_Weight<=1000
+order by x.turn desc limit 1
