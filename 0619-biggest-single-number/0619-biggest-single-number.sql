84bc0dd2701a5d71e883946eaf5e0f94708e230d
Sayan Saha <sahensha1600@gmail.com>
2024-12-02 21:35:05 +0530

    Time: 443 ms (51.07%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,6 @@
-select max(num) as num from MyNumbers where num in 
-(select num from MyNumbers group by num having count(*)=1)
+select max(x.num) as num from
+(
+    select num, count(*) as cnt from MyNumbers group by 1
+    order by cnt desc 
+) x
+where x.cnt=1
